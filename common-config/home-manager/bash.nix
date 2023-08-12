133e9abe62166582d3cc105ea018506388f8567d
Ryota <rytswd@gmail.com>
2023-08-12 22:13:46 +0100

    Correct path

--- common-config/home-manager/bash.nix
+++ common-config/home-manager/bash.nix
@@ -8,6 +8,6 @@
       # Any aliases specific for bash can be defined here.
 
       # Temporary alias for setting up Homebrew PATH for the current session.
-      brewsup = "export PATH=\"/opt/homebrew/bin:/opt/homebrew/sbin${PATH+:$PATH}\"";
+      brewsup = "export PATH=\"/opt/homebrew/bin:/opt/homebrew/sbin:${PATH+:$PATH}\"";
     };
 }

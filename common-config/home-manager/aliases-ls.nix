3f437a45fe4e7b04530a191547f0f6eee14abe3e
Ryota <rytswd@gmail.com>
2023-09-19 23:06:11 +0100

    Add directories first flag

--- common-config/home-manager/aliases-ls.nix
+++ common-config/home-manager/aliases-ls.nix
@@ -3,10 +3,10 @@
 {
   # Use default ls.
   # -G should handle the colouring, but zsh seems to have trouble with it.
-  ls  = "ls -F --color=auto";
-  l   = "ls -lF --color=auto";
-  la  = "ls -laF --color=auto";
-  ll  = "ls -ltrF --color=auto";
+  ls  = "ls -F --color=auto --group-directories-first";
+  l   = "ls -lF --color=auto --group-directories-first";
+  la  = "ls -laF --color=auto --group-directories-first";
+  ll  = "ls -ltrF --color=auto --group-directories-first";
 }
 # If exa is the preference, override the ls based commands. Otherwise, add
 # equivalent commands with an "x" prefix.

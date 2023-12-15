a28d44706ae8907543e24810a0e2c07c0ae746cc
Ryota <rytswd@gmail.com>
2023-12-15 15:04:47 +0000

    Add the latest kotlin grammar

--- common-config/overlays/tree-sitter.nix
+++ common-config/overlays/tree-sitter.nix
@@ -1,8 +1,11 @@
 final: prev:
 
 {
+  # Ref:
+  # https://github.com/NixOS/nixpkgs/tree/master/pkgs/development/tools/parsing/tree-sitter/grammars
   tree-sitter = prev.tree-sitter.override {
     extraGrammars = {
+      tree-sitter-kotlin = final.lib.importJSON ./tree-sitter-grammars/kotlin.json;
       tree-sitter-nu = final.lib.importJSON ./tree-sitter-grammars/nu.json;
       tree-sitter-templ = final.lib.importJSON ./tree-sitter-grammars/templ.json;
     };

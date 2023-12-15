c5fa5f29b2f118f8f776f4a844da361cea0f4ced
Ryota <rytswd@gmail.com>
2023-12-15 13:09:25 +0000

    Add templ grammar

--- common-config/overlays/tree-sitter.nix
+++ common-config/overlays/tree-sitter.nix
@@ -3,7 +3,8 @@ final: prev:
 {
   tree-sitter = prev.tree-sitter.override {
     extraGrammars = {
-      tree-sitter-nu = lib.importJSON ../tree-sitter-grammars/nu.json;
+      tree-sitter-nu = final.lib.importJSON ./tree-sitter-grammars/nu.json;
+      tree-sitter-templ = final.lib.importJSON ./tree-sitter-grammars/templ.json;
     };
   };
 }

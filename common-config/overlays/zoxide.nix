7fa9fda8940a471291566446f0af43aa42a0cb80
Ryota <rytswd@gmail.com>
2024-01-07 09:14:52 +0000

    Add a note about cargo hash

--- common-config/overlays/zoxide.nix
+++ common-config/overlays/zoxide.nix
@@ -11,4 +11,13 @@ final: prev:
       sha256 = "sha256-ut+/F7cQ5Xamb7T45a78i0mjqnNG9/73jPNaDLxzAx8=";
     };
   });
+
+  # Ref: https://discourse.nixos.org/t/is-it-possible-to-override-cargosha256-in-buildrustpackage/4393/3
+  # Ref: https://discourse.nixos.org/t/is-it-possible-to-override-cargosha256-in-buildrustpackage/4393/6
+  cargoDeps = old.cargoDeps.overrideAttrs (prev.lib.const {
+    name = "zoxide-vendor.tar.gz";
+    inherit src;
+    outputHash = ""; # Update this with cargoHash
+  });
+
 }

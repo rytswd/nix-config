1a0d5efb0317a9a0541566bd0a208328daf05f80
Ryota <rytswd@gmail.com>
2024-01-13 23:05:38 +0000

    Add neovim and helix
    For NeoVim, I took out most of the configs, as I am testing with
    NvChad for now.

--- user-config/ryota/home-manager.nix
+++ user-config/ryota/home-manager.nix
@@ -125,6 +125,9 @@
           # Emacs is configured at programs.emacs.
           # emacs-macport-rytswd  # Custom definition from overlay.
 
+          # NeoVim is configured at programs.neovim.
+          # Helix is configured at programs.helix.
+
           vscode
           vscode-insiders   # Added from the overlay setup
 
@@ -376,43 +379,49 @@
     # Most of the configs were taken from @micnncim 🥰
     neovim = {
       enable = true;
-      plugins = with pkgs; [
-        vimPlugins.copilot-vim
-        vimPlugins.nord-nvim
-        vimPlugins.nvim-treesitter
-        vimPlugins.nvim-treesitter-textobjects
-        vimPlugins.vim-markdown
-        {
-          plugin = vimPlugins.hop-nvim;
-          type = "lua";
-          config = ''
-            require("hop").setup { keys = 'uhetonas' } -- Dvorak
-            nmap("<Leader>w", ":HopWord<CR>")
-            nmap("<Leader>l", ":HopLine<CR>")
-            nmap("<Leader>s", ":HopChar1<CR>")
-          '';
-        }
-        {
-          plugin = vimPlugins.nvim-surround;
-          type = "lua";
-          config = ''
-            require("nvim-surround").setup()
-          '';
-        }
-      ];
-      extraConfig = ''
-        colorscheme nord
+      # NOTE: Commenting out the below setup, as I'm testing out NvChad for now.
+      # plugins = with pkgs; [
+      #   vimPlugins.copilot-vim
+      #   vimPlugins.nord-nvim
+      #   vimPlugins.nvim-treesitter
+      #   vimPlugins.nvim-treesitter-textobjects
+      #   vimPlugins.vim-markdown
+      #   {
+      #     plugin = vimPlugins.hop-nvim;
+      #     type = "lua";
+      #     config = ''
+      #       require("hop").setup { keys = 'uhetonas' } -- Dvorak
+      #       nmap("<Leader>w", ":HopWord<CR>")
+      #       nmap("<Leader>l", ":HopLine<CR>")
+      #       nmap("<Leader>s", ":HopChar1<CR>")
+      #     '';
+      #   }
+      #   {
+      #     plugin = vimPlugins.nvim-surround;
+      #     type = "lua";
+      #     config = ''
+      #       require("nvim-surround").setup()
+      #     '';
+      #   }
+      # ];
+      # extraConfig = ''
+      #   colorscheme nord
 
-        set number
+      #   set number
 
-        nnoremap <Space> <Nop>
-        let g:mapleader = "\<Space>"
-      '';
-      extraLuaConfig = ''
-        local function nmap(shortcut, command, opts)
-          vim.keymap.set("n", shortcut, command, opts or { noremap = true, silent = true })
-        end
-      '';
+      #   nnoremap <Space> <Nop>
+      #   let g:mapleader = "\<Space>"
+      # '';
+      # extraLuaConfig = ''
+      #   local function nmap(shortcut, command, opts)
+      #     vim.keymap.set("n", shortcut, command, opts or { noremap = true, silent = true })
+      #   end
+      # '';
+    };
+    helix = {
+      enable = true;
+
+      # Because Helix uses TOML for its configuration, I configure them in the
     };
 
     # + Terminals

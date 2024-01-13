be728efa6cd2ea5678d6b5bd177a2e4144e38fae
Ryota <rytswd@gmail.com>
2024-01-13 23:04:12 +0000

    Add astro tree-sitter grammar

--- common-config/overlays/tree-sitter.nix
+++ common-config/overlays/tree-sitter.nix
@@ -5,6 +5,7 @@ final: prev:
   # https://github.com/NixOS/nixpkgs/tree/master/pkgs/development/tools/parsing/tree-sitter/grammars
   tree-sitter = prev.tree-sitter.override {
     extraGrammars = {
+      tree-sitter-astro = final.lib.importJSON ./tree-sitter-grammars/astro.json;
       tree-sitter-kotlin = final.lib.importJSON ./tree-sitter-grammars/kotlin.json;
       tree-sitter-nu = final.lib.importJSON ./tree-sitter-grammars/nu.json;
       tree-sitter-templ = final.lib.importJSON ./tree-sitter-grammars/templ.json;

7c4e42c25265be8f66a130fe66bb5243b9b6c991
Ryota <rytswd@gmail.com>
2024-02-04 10:45:40 +0000

    Add a note about rust-analyzer with rustup

--- user-config/ryota/home-manager/common.nix
+++ user-config/ryota/home-manager/common.nix
@@ -92,6 +92,9 @@
           # it would be based on direnv setup, I'm using rustup as the default
           # instead. Nix setup here only handles the installation on rustup CLI
           # itself, and the rest is managed outside of Nix for simplicity.
+          # For rust-analyzer, I needed to run the following:
+          #   `rustup component add rust-analyzer`
+          # Not sure if this is a hard requirement, but this seems to work.
           rustup
           # rustc
           # cargo

f1f96307d578bb34c464435716b6d45646c0d976
Ryota <rytswd@gmail.com>
2024-02-17 18:39:24 +0000

    Add extra eza based ls

--- common-config/home-manager/aliases-ls.nix
+++ common-config/home-manager/aliases-ls.nix
@@ -14,12 +14,14 @@
   ls  = "eza --group-directories-first --classify";
   l   = "eza -l --group-directories-first --classify";
   la  = "eza -la --group-directories-first --classify";
+  lt  = "eza -lT --group-directories-first --classify";
   ll  = "eza -lT --group-directories-first --classify --sort modified";
   llt = "eza -laTF --git --group-directories-first --git-ignore --ignore-glob .git --sort modified";
 } else {
   xls = "eza --group-directories-first --classify";
   xl  = "eza -l --group-directories-first --classify";
   xla = "eza -la --group-directories-first --classify";
+  xlt  = "eza -lT --group-directories-first --classify";
   xll  = "eza -lT --group-directories-first --classify --sort modified";
   xllt = "eza -laTF --git --group-directories-first --git-ignore --ignore-glob .git --sort modified";
 })

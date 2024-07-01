c211025a91176146948a974a12e101ca12533cc0
Ryota <rytswd@gmail.com>
2024-07-01 09:49:29 +0100

    Change icons to be more subtle

--- user-config/ryota/home-manager/nixos.nix
+++ user-config/ryota/home-manager/nixos.nix
@@ -166,17 +166,17 @@ in {
           pulseaudio = {
             format = "{volume}% {icon} {format_source}";
             format-bluetooth = "{volume}% {icon} {format_source}";
-            format-bluetooth-muted = " {icon} {format_source}";
+            format-bluetooth-muted = "󰖁 {icon} {format_source}";
             format-icons = {
               car = "";
-              default = [ "" "" "" ];
+              default = [ "󰕿" "󰖀" "󰕾" ];
               handsfree = "";
-              headphones = "";
-              headset = "";
+              headphones = "󰋋";
+              headset = "󰋎";
               phone = "";
               portable = "";
             };
-            format-muted = " {format_source}";
+            format-muted = "󰖁 {format_source}";
             format-source = "{volume}% ";
             format-source-muted = "";
             on-click = "pavucontrol";

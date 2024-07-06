27f7276a6dc1e730b20028d73f22e2bb060d96c4
Ryota <rytswd@gmail.com>
2024-07-06 23:48:11 +0100

    Move all hyprland definitions into module

--- user-config/modules/window-manager/hyprland.nix
+++ user-config/modules/window-manager/hyprland.nix
@@ -2,6 +2,7 @@
 , lib
 , config
 , inputs
+, system
 , ...}:
 
 {
@@ -10,6 +11,34 @@
   };
 
   config = lib.mkIf config.window-manager.hyprland.enable {
+    wayland = {
+      windowManager = {
+        hyprland = {
+          enable = true;
+          # This assumes that the above XDG config is mapped to provide extra conf
+          # file, which can refer to as a relative path.
+          extraConfig = ''
+            source=./hyprland-custom.conf
+          '';
+          # TODO: Add extra handling so that extra files can be added based on
+          # the machine requirements (Asus will need specific resolution
+          # handling, whereas UTM won't need it.)
+          plugins = [
+            # inputs.hyprland-plugins.packages.${pkgs.stdenv.hostPlatform.system}.hyprlock
+          ];
+        };
+      };
+    };
+
+    # Because the config is quite lengthy, I'm simply mapping a file into the
+    # XDG directory.
+    xdg = {
+      configFile = {
+        "hypr/".source = ./hyprland-config;
+        "hypr/".recursive = true;
+      };
+    };
+
     home.packages = [
       inputs.hyprswitch.packages.${system}.default
     ];

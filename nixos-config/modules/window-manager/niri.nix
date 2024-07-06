9f68b875cd156b87a672fb1584a9bf75c738a175
Ryota <rytswd@gmail.com>
2024-07-06 12:54:09 +0100

    Add niri module

---
+++ nixos-config/modules/window-manager/niri.nix
@@ -0,0 +1,23 @@
+{ pkgs
+, lib
+, config
+, niri
+, ...}:
+
+{
+  options = {
+    window-manager.niri.enable = lib.mkEnableOption "Enable Niri window manager.";
+  };
+
+  config = lib.mkIf config.window-manager.niri.enable {
+    environment.systemPackage = [
+      pkgs.niri-unstable
+    ];
+
+    # NOTE: This can only be enabled once built with this stanza commented out.
+    programs.niri = {
+      enable = true;
+      package = pkgs.niri-unstable;
+    };
+  };
+}

b9dfabea09dd53d162d35c9e47ae89cd852797fb
Ryota <rytswd@gmail.com>
2024-07-06 12:52:04 +0100

    Adjust LESS to remove line numbers by default

--- user-config/ryota/home-manager/common.nix
+++ user-config/ryota/home-manager/common.nix
@@ -270,8 +270,15 @@
       # running. I could use `emacs -nw`, but for now, as my Emacs configuration
       # only works for GUI version, using nvim as the default makes it easy.
       EDITOR = "nvim";
-      LESS = "-iMNR";
       # TODO: Add DIRENV_LOG_FORMAT to be an empty string to suppress the output
+
+      # less
+      #   -i: Case insensitive
+      #   -M: Use long prompt
+      #   -N: Show line numbers
+      #   -R: Use raw input for colour escape sequence
+      # Line number is not used as of writing, as it is annoying for man.
+      LESS = "-iMR";
 
       # ripgrep
       RIPGREP_CONFIG_PATH = "${config.xdg.configHome}/ripgrep/config";

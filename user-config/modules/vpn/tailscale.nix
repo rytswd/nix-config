f4a79201ff0d19b528b6e5b76cccd51970dd08d5
Ryota <rytswd@gmail.com>
2024-07-08 10:19:21 +0100

    Correct wording

--- user-config/modules/vpn/tailscale.nix
+++ user-config/modules/vpn/tailscale.nix
@@ -5,10 +5,11 @@
 
 {
   options = {
-    vpn.tailscale.enable = lib.mkEnableOption "Enable ProtonVPN.";
+    vpn.tailscale.enable = lib.mkEnableOption "Enable Tailscale.";
   };
 
   config = lib.mkIf config.vpn.tailscale.enable {
+    services.tailscale.enable = true;
     home.packages = [ pkgs.tailscale ];
   };
 }

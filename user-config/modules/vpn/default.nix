4930f03d7056351324e7fa08591b0885fef34816
Ryota <rytswd@gmail.com>
2024-07-08 10:19:33 +0100

    Add clarification comment on tailscale

--- user-config/modules/vpn/default.nix
+++ user-config/modules/vpn/default.nix
@@ -5,12 +5,13 @@
 
 {
   imports = [
-    ./tailscale.nix
     ./protonvpn.nix
     # ./nordvpn.nix
   ];
 
-  vpn.tailscale.enable = lib.mkDefault true;
+  # NOTE: Tailscale requires the systemd setup, which is set up using
+  # services.tailscale.enable.
+  # vpn.tailscale.enable = lib.mkDefault true;
   vpn.protonvpn.enable = lib.mkDefault true;
   # vpn.nordvpn.enable = lib.mkDefault true;
 }

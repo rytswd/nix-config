3f939b339fb4346c8d0504c88edcf191f4165e64
Ryota <rytswd@gmail.com>
2024-07-08 10:22:17 +0100

    Remove tailscale from home-manager

--- user-config/modules/vpn/default.nix
+++ user-config/modules/vpn/default.nix
@@ -10,8 +10,8 @@
   ];
 
   # NOTE: Tailscale requires the systemd setup, which is set up using
-  # services.tailscale.enable.
-  # vpn.tailscale.enable = lib.mkDefault true;
+  # services.tailscale.enable for both NixOS and macOS.
+
   vpn.protonvpn.enable = lib.mkDefault true;
   # vpn.nordvpn.enable = lib.mkDefault true;
 }

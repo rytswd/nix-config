7e5c11371cb690d7622d91d28e63b793686e054a
Ryota <rytswd@gmail.com>
2024-07-09 17:53:00 +0100

    Add manual dictionary mapping for now

--- user-config/ryota/home-manager/common.nix
+++ user-config/ryota/home-manager/common.nix
@@ -418,6 +418,13 @@
 
       # Enchant config, and pull in
       "enchant/enchant.ordering".source = ../../../common-config/enchant/enchant.ordering;
+      "enchant/hunspell/en_US.aff".source = "${pkgs.hunspellDicts.en_US-large}/share/hunspell/en_US.aff";
+      "enchant/hunspell/en_US.dic".source = "${pkgs.hunspellDicts.en_US-large}/share/hunspell/en_US.dic";
+      "enchant/hunspell/en_GB.aff".source = "${pkgs.hunspellDicts.en_GB-large}/share/hunspell/en_GB.aff";
+      "enchant/hunspell/en_GB.dic".source = "${pkgs.hunspellDicts.en_GB-large}/share/hunspell/en_GB.dic";
+      "enchant/aspell/en-computers.rws".source = "${pkgs.aspellDicts.en-computers}/lib/aspell/en-computers.rws";
+      "enchant/aspell/en_US-science.rws".source = "${pkgs.aspellDicts.en-science}/lib/aspell/en_US-science.rws";
+      "enchant/aspell/en_GB-science.rws".source = "${pkgs.aspellDicts.en-science}/lib/aspell/en_GB-science.rws";
 
       "fd".source = ../../../common-config/fd;
       "fd".recursive = true;

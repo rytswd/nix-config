8093f97b8c59241dc48f559c1d5fc40cdd349606
Ryota <rytswd@gmail.com>
2024-07-11 00:02:09 +0100

    Correct inputs and imports

--- user-config/modules/bar/ags.nix
+++ user-config/modules/bar/ags.nix
@@ -1,15 +1,16 @@
 { pkgs
 , lib
 , config
+, inputs
 , ...}:
 
 {
+  imports = [ inputs.ags.homeManagerModules.default ];
   options = {
     bar.ags.enable = lib.mkEnableOption "Enable AGS (Aylur's GTK Shell).";
   };
 
   config = lib.mkIf config.bar.ags.enable {
-    imports = [ inputs.ags.homeManagerModules.default ];
     programs.ags = {
       enable = true;
       configDir = ./ags;

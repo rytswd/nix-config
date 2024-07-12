5d62a31d65504c72ce5ed9f5db15c4c66478db0c
Ryota <rytswd@gmail.com>
2024-07-12 17:13:51 +0100

    Correct AGS based notification handling

--- user-config/modules/notification/ags-notification.nix
+++ user-config/modules/notification/ags-notification.nix
@@ -5,18 +5,17 @@
 , ...}:
 
 {
-  imports = [ inputs.ags.homeManagerModules.default ];
   options = {
-    bar.ags.enable = lib.mkEnableOption "Enable notification handling based on AGS (Aylur's GTK Shell).";
+    notification.ags-notification.enable = lib.mkEnableOption "Enable notification handling based on AGS (Aylur's GTK Shell).";
   };
 
-  config = lib.mkIf config.bar.ags.enable {
-    home.packages = {
-      inputs.ags.packages.${pkgs.system}.default;
-    };
+  config = lib.mkIf config.notification.ags-notification.enable {
+    home.packages = [
+      inputs.ags.packages.${pkgs.system}.default
+    ];
     xdg.configFile = {
-      "ags/notification".source = ./ags-notification;
-      "ags/notification".recursive = true;
+      "ags-notification".source = ./ags-notification;
+      "ags-notification".recursive = true;
     };
   };
 }
--- user-config/modules/notification/default.nix
+++ user-config/modules/notification/default.nix
@@ -4,11 +4,16 @@
 , ...}:
 
 {
+  # NOTE: AGS (Aylur's GTK Shell) can provide notification handling. While I
+  # could bundle all the AGS modules into a single setup, I'm purposely making
+  # each module as a separate entry for now.
   imports = [
     ./dunst.nix
     ./wired.nix
+    ./ags-notification.nix
   ];
 
   notification.dunst.enable = lib.mkDefault false; # Being explicit
   notification.wired.enable = lib.mkDefault false; # Being explicit
+  notification.ags-notification.enable = lib.mkDefault false; # Being explicit
 }

e2c414427891358b2d46ceddfe77cb63c75528bf
Ryota <rytswd@gmail.com>
2024-07-13 01:17:28 +0100

    Ensure to add direnv and starship

--- user-config/modules/shell/default.nix
+++ user-config/modules/shell/default.nix
@@ -11,11 +11,13 @@
     # earlier.
     ./aliases.nix
     ./env.nix
+    ./direnv.nix
     ./util.nix
     ./bash.nix
     ./fish.nix
     ./nushell.nix
     ./zsh.nix
+    ./starship.nix
 
     # I don't use it much, but keeping it in.
     ./tmux.nix
@@ -24,11 +26,13 @@
   shell.standard.enable = lib.mkDefault true;
   shell.aliases.enable = lib.mkDefault true;
   shell.env.enable = lib.mkDefault true;
+  shell.direnv.enable = lib.mkDefault true;
   shell.util.enable = lib.mkDefault true;
   shell.bash.enable = lib.mkDefault true;
   shell.fish.enable = lib.mkDefault true;
   shell.nushell.enable = lib.mkDefault true;
   shell.zsh.enable = lib.mkDefault true;
+  shell.starship.enable = lib.mkDefault true;
 
   shell.tmux.enable = lib.mkDefault false; # Being explicit
 }

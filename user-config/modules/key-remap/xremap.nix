e185be668593f7e0ecea682071271851de4871c8
Ryota <rytswd@gmail.com>
2024-07-16 21:41:30 +0100

    Adjust xremap config
    The current setting here uses the bare minimum of what Xremap is
    capable of, as it's currently failing to find the application based on
    wlroots binding.

--- user-config/modules/key-remap/xremap.nix
+++ user-config/modules/key-remap/xremap.nix
@@ -5,17 +5,21 @@
 , ...}:
 
 {
-  imports = [ inputs.xremap.homeManagerModules.default ];
   options = {
     key-remap.xremap.enable = lib.mkEnableOption "Enable xremap.";
   };
+
+  imports = [ inputs.xremap.homeManagerModules.default ];
 
   config = lib.mkIf config.key-remap.xremap.enable {
     services.xremap = {
       enable = true;
       withWlroots = true;
       watch = true;
-      yamlConfig = ./xremap.yaml;
+      deviceNames = [
+        "Asus\ Keyboard"
+      ];
+      yamlConfig = (builtins.readFile ./xremap.yaml);
     };
   };
 }

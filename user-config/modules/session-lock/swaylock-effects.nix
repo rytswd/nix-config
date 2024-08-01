a9e0095e45e87a6989f2497f727e59ee053d3ddd
Ryota <rytswd@gmail.com>
2024-08-01 03:34:06 +0100

    Add swaylock config correctly

--- user-config/modules/session-lock/swaylock-effects.nix
+++ user-config/modules/session-lock/swaylock-effects.nix
@@ -12,22 +12,8 @@
     home.packages = [
       pkgs.swaylock-effects
     ];
-    home.shellAliases = {
-      "swaylock-effects" = "swaylock"
-                           + " --screenshots"
-                           + " --clock"
-                           + " --indicator"
-                           + " --indicator-radius 70"
-                           + " --indicator-thickness 4"
-                           + " --effect-blur 7x5"
-                           + " --effect-vignette 0.5:0.5"
-                           + " --ring-color 006BA6"
-                           + " --key-hl-color 0496FF"
-                           + " --line-color 00000000"
-                           + " --inside-color 00000088"
-                           + " --separator-color 00000000"
-                           + " --grace 1"
-                           + " --fade-in 0.2";
+    xdg.configFile = {
+      "swaylock/config".source = ./swaylock/config;
     };
   };
 }

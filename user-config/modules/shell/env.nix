3b0c88eee539e7be2c985ec30ce01075e5b564a1
Ryota <rytswd@gmail.com>
2024-08-15 13:29:58 +0100

    Adjust wording

--- user-config/modules/shell/env.nix
+++ user-config/modules/shell/env.nix
@@ -13,7 +13,7 @@
       # Although I use Emacs for my main driver, I want to ensure that this
       # editor choice works in any environment, even when Emacs server is not
       # running. I could use `emacs -nw`, but for now, as my Emacs configuration
-      # only works for GUI version, using nvim as the default makes it easy.
+      # only works for GUI version, using nvim as the default works for me.
       EDITOR = "nvim";
 
       # less

f42fa3d47fd6266c788ee5d30b22c735266e4626
Ryota <rytswd@gmail.com>
2024-08-27 23:54:54 +0100

    Add extra field to test drag behaviour

--- nixos-config/modules/input/trackpad.nix
+++ nixos-config/modules/input/trackpad.nix
@@ -9,9 +9,15 @@
   };
 
   config = lib.mkIf config.input.trackpad.enable {
-    # Disable the touchpad while typing.
-    # NOTE: This works by itself, but other key remapping solutions like xremap
-    # would change the behaviour and may not have any effect because of that.
-    services.libinput.touchpad.disableWhileTyping = true;
+    services.libinput.touchpad = {
+      # TODO: I was hpoing this would work for screenshot dragging, but not
+      # really. I need to sort this out.
+      tappingDragLock = true;
+      # Disable the touchpad while typing.
+      # NOTE: This works by itself, but other key remapping solutions like
+      # xremap would change the behaviour and may not have any effect because
+      # of that.
+      disableWhileTyping = true;
+    };
   };
 }

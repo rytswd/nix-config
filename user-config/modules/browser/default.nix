d098d603a88cb0d45552c542cc18d49415cf76b2
Ryota <rytswd@gmail.com>
2024-08-28 23:47:41 +0100

    Add Nyxt but keep it disabled
    The build fails when built with NixOS, though it works fine with the
    nix profile install. For now, I'll use the nix profile version.

--- user-config/modules/browser/default.nix
+++ user-config/modules/browser/default.nix
@@ -7,8 +7,9 @@
   imports = [
     ./brave.nix
     ./vivaldi.nix
-    ./nyxt.nix
     ./chromium.nix
+    # TODO: Nyxt build fails, and thus disabling it for now.
+    # ./nyxt.nix
   ];
 
   # NOTE: The browsers are assumed to be the setup for NixOS. This is because
@@ -16,6 +17,6 @@
   # Arc Browser instead.
   browser.brave.enable = lib.mkDefault true;
   browser.vivaldi.enable = lib.mkDefault true;
-  browser.nyxt.enable = lib.mkDefault true;
   # browser.chromium.enable = lib.mkDefault true;
+  # browser.nyxt.enable = lib.mkDefault true;
 }

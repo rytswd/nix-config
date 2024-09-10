4426f2125f3b1589615242db7bf91d6dcc78f8ba
Ryota <rytswd@gmail.com>
2024-09-10 05:12:10 +0100

    Add a few aliases for Nushell
    Nushell has a specific setup for aliases, which currently does not
    work well with the existing home-manager alias support. I'm making
    some of the common aliases I use explicitly for Nushell, as I do use
    Nushell for a lot of debugging.

--- user-config/modules/shell/nushell.nix
+++ user-config/modules/shell/nushell.nix
@@ -14,6 +14,14 @@
       shellAliases = (import ./aliases-ls.nix { withEza = true; }) //
         {
           # Any aliases specific for fish can be defined here.
+          # NOTE: Because of the way Nushell aliases work, Nushell cannot make
+          # use of the `home.shellAliases` like other shells. I need to list
+          # out all the aliases I use here instead.
+          # Ref: https://github.com/nix-community/home-manager/pull/4616#issuecomment-1817812397
+          k = "kubectl";
+          gccact = "gcloud config configurations activate";
+          gccls = "gcloud config configurations list";
+          tf = "terraform";
         };
    };
   };

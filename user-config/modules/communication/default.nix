474f79d4ac85d9a904870315da0a6a2a8b3f916d
Ryota <rytswd@gmail.com>
2024-09-17 15:16:22 +0100

    Add zoom

--- user-config/modules/communication/default.nix
+++ user-config/modules/communication/default.nix
@@ -9,10 +9,12 @@
     ./slack.nix
     ./signal.nix
     ./discord.nix
+    ./zoom.nix
   ];
 
   communication.email.enable = lib.mkDefault true;
   communication.slack.enable = lib.mkDefault false; # Being explicit
   communication.signal.enable = lib.mkDefault false; # Being explicit
   communication.discord.enable = lib.mkDefault false; # Being explicit
+  communication.zoom.enable = lib.mkDefault false; # Being explicit
 }
---
+++ user-config/modules/communication/zoom.nix
@@ -0,0 +1,16 @@
+{ pkgs
+, lib
+, config
+, ...}:
+
+{
+  options = {
+    communication.zoom.enable = lib.mkEnableOption "Enable Zoom.";
+  };
+
+  config = lib.mkIf config.communication.zoom.enable {
+    # Instead of using the official Discord app, I'm making use of Vesktop.
+    # Discord seems to have less support around Wayland environment.
+    home.packages = [ pkgs.zoom-us ];
+  };
+}

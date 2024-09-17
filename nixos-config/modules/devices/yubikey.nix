816e4d80b8ff5da000bf0bfad797713585996c40
Ryota <rytswd@gmail.com>
2024-09-17 22:13:52 +0100

    Add U2F and YubiKey removal to lock

--- nixos-config/modules/devices/yubikey.nix
+++ nixos-config/modules/devices/yubikey.nix
@@ -18,6 +18,42 @@
     services.udev.packages = [ pkgs.yubikey-personalization ];
     services.pcscd.enable = true;
 
+    # Lock when the YubiKey is unplugged.
+    # This works with YubiKey 5C NFC. It may need to be adjusted for other
+    # models.
+    #
+    # Ref: https://nixos.wiki/wiki/Yubikey#Locking_the_screen_when_a_Yubikey_is_unplugged
+    services.udev.extraRules = ''
+      ACTION=="remove",\
+       ENV{ID_BUS}=="usb",\
+       ENV{ID_MODEL_ID}=="0407",\
+       ENV{ID_VENDOR_ID}=="1050",\
+       ENV{ID_VENDOR}=="Yubico",\
+       RUN+="${pkgs.systemd}/bin/loginctl lock-sessions"
+    '';
+
+    # Enable U2F for login.
+    #
+    # Commands I can run to generate:
+    #     nix shell nixpkgs#pam_u2f
+    #
+    #     # 1st YubiKey
+    #     pamu2fcfg     > ~/.config/Yubico/u2f_keys
+    #     # 2nd YubiKey
+    #     pamu2fcfg -n >> ~/.config/Yubico/u2f_keys
+    #
+    # Command to test:
+    #     nix shell nixpkgs#pamtester
+    #
+    #     pamtester login ryota authenticate
+    #     pamtester sudo  ryota authenticate
+    #
+    # Ref: https://nixos.wiki/wiki/Yubikey#pam_u2f
+    security.pam.services = {
+      login.u2fAuth = true;
+      sudo.u2fAuth = true;
+    };
+
     # https://github.com/FiloSottile/yubikey-agent?tab=readme-ov-file
     # Because I am using GPG agent at the moment, I'm taking this out. There
     # seems to be some benefits for using yubikey-agent over gpg-agent. While I

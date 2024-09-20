a1d99ebab7167480e187152e02c0f519fb249627
Ryota <rytswd@gmail.com>
2024-09-20 02:35:38 +0100

    Correct libinput quirks

--- nixos-config/modules/devices/keyboard.nix
+++ nixos-config/modules/devices/keyboard.nix
@@ -12,11 +12,11 @@
     # HACK: I shouldn't need this, but making sure any keyboard input device
     # is considered for the dwt (disable while typing) functionality from
     # libinput.
-    environment.etc."libinput/local-overrides.quirks" =
+    environment.etc."libinput/local-overrides.quirks".text =
       ''
         [xremap]
         MatchUdevType=keyboard
-        AttrKeyboardIntegration=interna
+        AttrKeyboardIntegration=internal
       '';
   };
 }

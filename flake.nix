a90fe267d3053d640bd9a69d6e7c7208b9a13123
Ryota <rytswd@gmail.com>
2024-09-20 02:36:41 +0100

    Ensure to use the same nixpkgs input

--- flake.nix
+++ flake.nix
@@ -24,7 +24,7 @@
 
     sops-nix = {
       url = "github:Mic92/sops-nix";
-      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.nixpkgs.follows = "nixpkgs-unstable";
     };
 
     ###----------------------------------------

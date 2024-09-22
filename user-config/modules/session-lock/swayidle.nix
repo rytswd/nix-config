6376549c18337b7172ba315ac4f6f4ff91a4183c
Ryota <rytswd@gmail.com>
2024-09-22 10:59:24 +0100

    Add a note

--- user-config/modules/session-lock/swayidle.nix
+++ user-config/modules/session-lock/swayidle.nix
@@ -18,6 +18,7 @@
         # { event = "after-resume"; command = "${pkgs.sway}/bin/swaymsg \"output * toggle\""; }
       ];
       timeouts = [
+        # 15 min
         { timeout = (60 * 15); command = "${pkgs.swaylock-effects}/bin/swaylock"; }
         # { timeout = 1200; command = "${pkgs.sway}/bin/swaymsg \"output * toggle\""; }
       ];

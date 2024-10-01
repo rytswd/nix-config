14f7d13fb92eba259e8f565ec2f3cc1d8079e7d0
Ryota <rytswd@gmail.com>
2024-10-01 02:22:28 +0100

    Enable wlogout

--- user-config/modules/session-lock/default.nix
+++ user-config/modules/session-lock/default.nix
@@ -7,8 +7,10 @@
   imports = [
     ./swayidle.nix
     ./swaylock-effects.nix
+    ./wlogout
   ];
 
   session-lock.swayidle.enable = lib.mkDefault true;
   session-lock.swaylock-effects.enable = lib.mkDefault true;
+  session-lock.wlogout.enable = lib.mkDefault true;
 }

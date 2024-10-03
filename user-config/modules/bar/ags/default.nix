3c67d0b2de8399068ad60f3096eaddef785fd327
Ryota <rytswd@gmail.com>
2024-10-03 23:00:55 +0100

    Add ags back

---
+++ user-config/modules/bar/ags/default.nix
@@ -0,0 +1,19 @@
+{ pkgs
+, lib
+, config
+, inputs
+, ...}:
+
+{
+  imports = [ inputs.ags.homeManagerModules.default ];
+  options = {
+    bar.ags.enable = lib.mkEnableOption "Enable AGS (Aylur's GTK Shell).";
+  };
+
+  config = lib.mkIf config.bar.ags.enable {
+    programs.ags = {
+      enable = true;
+      configDir = ./config;
+    };
+  };
+}

bc0a94cc7901d56494d2bf6d20a4022a31b8757f
Ryota <rytswd@gmail.com>
2024-10-03 22:52:38 +0100

    Make window-manager selection false by default

--- user-config/modules/window-manager/default.nix
+++ user-config/modules/window-manager/default.nix
@@ -7,8 +7,10 @@
   imports = [
     ./hyprland
     ./niri
+    ./yabai
   ];
 
-  window-manager.hyprland.enable = lib.mkDefault true;
-  window-manager.niri.enable = lib.mkDefault true;
+  window-manager.hyprland.enable = lib.mkDefault false; # Being explicit
+  window-manager.niri.enable = lib.mkDefault false; # Being explicit
+  window-manager.yabai.enable = lib.mkDefault false; # Being explicit
 }
--- user-config/modules/window-manager/niri/default.nix
+++ user-config/modules/window-manager/niri/default.nix
@@ -11,7 +11,7 @@
 
   config = lib.mkIf config.window-manager.niri.enable {
     # Because the config is quite lengthy, I'm simply mapping a file into the
-    # XDG directory.
+    # XDG directory. All the code is generated with the Org Mode tangle.
     xdg.configFile = {
       "niri/config.kdl".source = ./config.kdl;
     };

c1da4899f98a8aa866655161c83ef141879151c9
Ryota <rytswd@gmail.com>
2024-10-04 15:54:25 +0100

    Add clarification comment about seahorse

--- user-config/modules/linux-widget/password.nix
+++ user-config/modules/linux-widget/password.nix
@@ -11,7 +11,7 @@
   config = lib.mkIf config.linux-widget.password.enable {
     home.packages = [
       # NOTE: Seahorse is "Passwords and Keys" app from GNOME. It used to be a
-      # part of pkgs.gnome, but now it's a top-level application.
+      # part of pkgs.gnome, but now it's a top-level application from Nix 24.11.
       pkgs.seahorse
     ];
   };

80e9dc8f4f8a86a131fc9d70f1bc5b3eea42e77a
Ryota <rytswd@gmail.com>
2024-10-04 16:01:32 +0100

    Add networkmanager without GNOME dependency

--- nixos-config/modules/core/network.nix
+++ nixos-config/modules/core/network.nix
@@ -13,5 +13,8 @@
     # Per-interface useDHCP will be mandatory in the future, so this generated config
     # replicates the default behaviour.
     networking.useDHCP = false;
+    # Ensure network manager is a part of the config even when I don't have any
+    # desktop manager like GNOME.
+    networking.networkmanager.enable = true;
   };
 }

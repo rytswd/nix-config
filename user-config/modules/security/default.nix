ee0e3b504c818b6912f2480fbd65be36383ebf97
Ryota <rytswd@gmail.com>
2024-10-05 02:24:43 +0100

    Add note about SOPS usage

--- user-config/modules/security/default.nix
+++ user-config/modules/security/default.nix
@@ -7,6 +7,8 @@
   imports = [
     ./age.nix
     ./gpg.nix
+    # NOTE: Not using sops for now.
+    # ./sops.nix # NOTE: Importing this would enable regardless.
   ];
 
   security.age.enable = lib.mkDefault true;

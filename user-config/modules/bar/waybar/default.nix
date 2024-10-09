ffca4a67bee992531015f06383fe3e3244e3b1d5
Ryota <rytswd@gmail.com>
2024-10-09 21:07:52 +0100

    Update to create multiple systemd

--- user-config/modules/bar/waybar/default.nix
+++ user-config/modules/bar/waybar/default.nix
@@ -12,17 +12,71 @@
     programs.waybar = {
       enable = true;
 
-      # TODO: I may need to reconsider this approach, and create a separate /
-      # dedicated systemd setup as I want to have both top and bottom bars.
-      systemd.enable = true;
+      # I'm making an explicit systemd entry as per below.
+      systemd.enable = false;
 
       # NOTE: I'm not using the Nix based definitions as it was easier to test
       # using direct jsonc and css inputs.
     };
+
     xdg.configFile = {
-      # NOTE: Creating top configs only for now.
-      "waybar/config".source = ./config_top.jsonc;
-      "waybar/style.css".source = ./style_top.css;
+      "waybar".source = ./config;
+      "waybar".recursive = true;
+    };
+
+    systemd.user.services = {
+      waybar_top = {
+        Unit.Description = "Waybar for top part.";
+
+        Service = {
+          Restart = "on-failure";
+          ExecStart = ''
+          ${pkgs.waybar}/bin/waybar \
+            -c ${config.xdg.configHome}/waybar/config_top.jsonc \
+            -s ${config.xdg.configHome}/waybar/style_top.css
+          '';
+          ExecReload = "${pkgs.coreutils}/bin/kill -SIGUSR2 $MAINPID";
+          KillMode = "mixed";
+        };
+
+        Install.WantedBy = [ "default.target" ];
+      };
+
+      waybar_bottom = {
+        Unit.Description = "Waybar for bottom part.";
+
+        Service = {
+          Restart = "on-failure";
+          ExecStart = ''${pkgs.waybar}/bin/waybar \
+            -c ${config.xdg.configHome}/waybar/config_bottom.jsonc \
+            -s ${config.xdg.configHome}/waybar/style_bottom.css'';
+          ExecReload = "${pkgs.coreutils}/bin/kill -SIGUSR2 $MAINPID";
+          KillMode = "mixed";
+        };
+
+        Install.WantedBy = [ "default.target" ];
+      };
+
+      waybar_left = {
+        Unit = {
+          Description = "Waybar for left part.";
+          # Because left bar can affect the look of the top and bottom, I am
+          # ensuring that the top and bottom bars are handled first.
+          After = [ "waybar_top.service" "waybar_bottom.service" ];
+          Requires = [ "waybar_top.service" "waybar_bottom.service" ];
+        };
+
+        Service = {
+          Restart = "on-failure";
+          ExecStart = ''${pkgs.waybar}/bin/waybar \
+            -c ${config.xdg.configHome}/waybar/config_left.jsonc \
+            -s ${config.xdg.configHome}/waybar/style_left.css'';
+          ExecReload = "${pkgs.coreutils}/bin/kill -SIGUSR2 $MAINPID";
+          KillMode = "mixed";
+        };
+
+        Install.WantedBy = [ "default.target" ];
+      };
     };
   };
 }

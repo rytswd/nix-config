29f6a7232fd351cb0ef21e49338a6a0f7ee9eb8a
Ryota <rytswd@gmail.com>
2024-10-22 12:15:35 +0100

    Use version 4 for macos

--- macos-config/mbp/configuration.nix
+++ macos-config/mbp/configuration.nix
@@ -254,11 +254,7 @@ in {
     };
   };
 
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "24.05"; # Did you read the comment?
+  # The system version works differently for macOS.
+  # Ref: https://github.com/LnL7/nix-darwin/issues/1087
+  system.stateVersion = 4;
 }

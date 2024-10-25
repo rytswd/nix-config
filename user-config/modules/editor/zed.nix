4cf9d6a4b7d9f33203f4effffab26b2f99b76b60
Ryota <rytswd@gmail.com>
2024-10-25 17:17:20 +0100

    Use zed editor program

--- user-config/modules/editor/zed.nix
+++ user-config/modules/editor/zed.nix
@@ -9,13 +9,8 @@
   };
 
   config = lib.mkIf config.editor.zed.enable {
-    # Pending on https://github.com/nix-community/home-manager/pull/5455
-    # programs.zed-editor = {
-    #   enable = true;
-    # };
-
-    home.packages = [
-      pkgs.zed-editor
-    ];
+    programs.zed-editor = {
+      enable = true;
+    };
   };
 }

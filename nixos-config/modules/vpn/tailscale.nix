7d07b5cdc7100c3a264041b9e7ca4140af3536ff
Ryota <rytswd@gmail.com>
2024-11-06 14:09:43 +0000

    Add comment about tailscale service

--- nixos-config/modules/vpn/tailscale.nix
+++ nixos-config/modules/vpn/tailscale.nix
@@ -11,6 +11,9 @@
   config = lib.mkIf config.vpn.tailscale.enable {
     services.tailscale = {
       enable = true;
+      # TODO: This is done by running the oneoff command via systemd, but does
+      # not get rerun when rebuilding the system. There may be a better way to
+      # handle this.
       extraSetFlags = [
         "--operator=ryota"
       ];

31e0c3da801dafa030ec29db9a60bcd60ef05378
Ryota <rytswd@gmail.com>
2024-11-19 15:36:52 +0000

    fixup! Add rofi

--- user-config/modules/launcher/default.nix
+++ user-config/modules/launcher/default.nix
@@ -6,10 +6,12 @@
 {
   imports = [
     ./fuzzel.nix
+    ./rofi.nix
     ./wofi.nix
   ];
 
   # Fuzzel works better with Niri, and thus making it the default.
   launcher.fuzzel.enable = lib.mkDefault true;
+  launcher.rofi.enable = lib.mkDefault true;
   launcher.wofi.enable = lib.mkDefault false; # Being explicit
 }
--- user-config/modules/launcher/rofi.nix
+++ user-config/modules/launcher/rofi.nix
@@ -1,7 +1,6 @@
 { pkgs
 , lib
 , config
-, inputs
 , ...}:
 
 {
@@ -13,14 +12,22 @@
     programs.rofi = {
       enable = true;
       package = pkgs.rofi-wayland;
-      plugins = [
-        pkgs.rofi-emoji
-      ];
+      # Because I use bemoji, I don't really need rofi-emoji plugin.
+      # plugins = [
+      #   pkgs.rofi-emoji-wayland
+      # ];
       extraConfig = {
         show-icons = true;
-        modi = "drun,emoji,ssh";
+        modi = "window,drun";
+        terminal = "ghostty";
+        combi-hide-mode-prefix = true;
       };
+      # Because of the CSS like input, it's easier to target a file, and map it
+      # with XDG config.
+      theme = "custom.rasi";
+    };
+    xdg.configFile = {
+      "rofi/themes/custom.rasi".source = ./rofi-theme.rasi;
     };
   };
-
 }

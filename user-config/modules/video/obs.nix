d114104087a2563b51ff2d83e076a5cafe4e3c6e
Ryota <rytswd@gmail.com>
2024-12-03 12:47:44 +0000

    Add obs-advanced-masks with my own derivation

--- user-config/modules/video/obs.nix
+++ user-config/modules/video/obs.nix
@@ -11,14 +11,18 @@
   config = lib.mkIf config.video.obs.enable {
     programs.obs-studio = {
       enable = true;
-      plugins = [
+      plugins = let
+        # Pending PR https://github.com/NixOS/nixpkgs/pull/360018
+        obs-advanced-masks = pkgs.callPackage ./obs-advanced-masks.nix {};
+      in [
         pkgs.obs-studio-plugins.obs-gradient-source
         pkgs.obs-studio-plugins.obs-composite-blur
         pkgs.obs-studio-plugins.obs-3d-effect
         pkgs.obs-studio-plugins.obs-source-clone
         pkgs.obs-studio-plugins.obs-move-transition
 
-        # Add OBS advanced mask?
+        # Custom
+        obs-advanced-masks
       ];
     };
   };

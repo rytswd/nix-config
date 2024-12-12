f6578da125433fd8d2d478a313ec09476914afbe
Ryota <rytswd@gmail.com>
2024-12-12 03:46:29 +0000

    Comment out sql grammar which is acting odd

--- overlays/tree-sitter.nix
+++ overlays/tree-sitter.nix
@@ -9,7 +9,7 @@ final: prev:
       tree-sitter-kotlin = final.lib.importJSON ./tree-sitter-grammars/kotlin.json;
       tree-sitter-nu = final.lib.importJSON ./tree-sitter-grammars/nu.json;
       tree-sitter-templ = final.lib.importJSON ./tree-sitter-grammars/templ.json;
-      tree-sitter-sql = final.lib.importJSON ./tree-sitter-grammars/sql.json;
+      # tree-sitter-sql = final.lib.importJSON ./tree-sitter-grammars/sql.json;
       tree-sitter-roc = final.lib.importJSON ./tree-sitter-grammars/roc.json;
       tree-sitter-hcl = final.lib.importJSON ./tree-sitter-grammars/hcl.json;
     };

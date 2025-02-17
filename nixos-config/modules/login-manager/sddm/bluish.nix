f14b87e4c7f2e68a3d68df4db48c3e22939a6b82
Ryota <rytswd@gmail.com>
2025-02-17 14:13:05 +0000

    Add sddm adjustment attempt

--- nixos-config/modules/login-manager/sddm/bluish.nix
+++ nixos-config/modules/login-manager/sddm/bluish.nix
@@ -1,7 +1,13 @@
-{ stdenv
-, fetchFromGitHub }:
-
-stdenv.mkDerivation {
+{ lib
+, stdenvNoCC
+, fetchFromGitHub
+, wrapQtAppsHook
+, qtbase
+, qtsvg
+, qtgraphicaleffects
+, qtquickcontrols2
+}:
+stdenvNoCC.mkDerivation {
   pname = "sddm-bluish";
   version = "1.0.0"; # Any version information
   src = fetchFromGitHub {
@@ -10,8 +16,19 @@ stdenv.mkDerivation {
     rev = "0ff89cb290811c8113b5d91b8cdfa772df10f926";
     sha256 = "sha256-aJA5vPcrm3PVrWzw+hyKB5eH4wCU3EH+nWlElPjM9eo=";
   };
-
   dontBuild = true;
+  
+  nativeBuildInputs = [
+    wrapQtAppsHook
+  ];
+
+  propagatedUserEnvPkgs = [
+    qtbase
+    qtsvg
+    qtgraphicaleffects
+    qtquickcontrols2
+  ];
+  
   installPhase = ''
     mkdir -p $out/share/sddm/themes
     cp -aR $src/Bluish\ SDDM\ Themes/Bluish-SDDM-6 $out/share/sddm/themes/bluish
--- nixos-config/modules/login-manager/sddm/default.nix
+++ nixos-config/modules/login-manager/sddm/default.nix
@@ -11,17 +11,22 @@
   config = lib.mkIf config.login-manager.sddm.enable {
     environment.systemPackages = let
       # NOTE: Extra theme setup by pulling GitHub repos.
-      tokyo-night = pkgs.callPackage ./tokyo-night.nix { };
-      bluish = pkgs.callPackage ./bluish.nix { };
+      # These will be placed under /run/current-system/sw/share/sddm/themes.
+      tokyo-night = pkgs.libsForQt5.callPackage ./tokyo-night.nix { };
+      # TODO: tokyo-night is the only one that's working correctly...
+      # swish = pkgs.libsForQt5.callPackage ./swish.nix { };
+      bluish = pkgs.libsForQt5.callPackage ./bluish.nix { };
       in [
         tokyo-night
+        # swish
         bluish
       ];
     services.displayManager.sddm = {
       enable = true;
       # theme = "maldives";
-      # theme = "tokyo-night";
-      theme = "bluish";
+      theme = "tokyo-night";
+      # theme = "swish";
+      # theme = "bluish";
     };
   };
 }

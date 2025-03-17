10643c8690cba02492284bc31ed63930a1cb669a
Ryota <rytswd@gmail.com>
2025-03-17 13:25:58 +0000

    Add uutils

--- user-config/modules/shell/standard.nix
+++ user-config/modules/shell/standard.nix
@@ -11,7 +11,14 @@
   config = lib.mkIf config.shell.standard.enable {
     # NOTE: Some tools such as git has dedicated modules defined to pull in.
     home.packages = [
+      # NOTE: I could keep the GNU coreutils, or use uutils (rewritten in Rust)
+      # version instead. When using pkgs.uutils-coreutils, I get all the uutils
+      # utilities which are prefixed with "uutils-". If I want to fully replace
+      # the coreutils tools, I could use pkgs.uutils-coreutils-noprefix.
       pkgs.coreutils
+      pkgs.uutils-coreutils
+      # pkgs.uutils-coreutils-noprefix
+
       pkgs.curl
       pkgs.jq
       pkgs.tree

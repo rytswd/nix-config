b28d38feba5d60054ee64dc8f7c38930f7419bd9
Ryota <rytswd@gmail.com>
2025-03-17 03:50:43 +0000

    Use more binary cache

--- nixos-config/asus-rog-zephyrus-g14-2024/nix-flags.nix
+++ nixos-config/asus-rog-zephyrus-g14-2024/nix-flags.nix
@@ -40,26 +40,20 @@
       substituters = [
         "https://cache.nixos.org/"
         "https://nix-community.cachix.org"
-        # "https://rytswd-nix-config.cachix.org"
-        # "https://ghostty.cachix.org"
-        # "https://cosmic.cachix.org"
-
-        # "https://niri.cachix.org"
       ];
       extra-substituters = [
         "https://rytswd-nix-config.cachix.org"
         "https://ghostty.cachix.org"
         "https://cosmic.cachix.org"
 
-        # "https://niri.cachix.org"
+        "https://hyprland.cachix.org"
+        "https://niri.cachix.org"
+        "https://roc-lang.cachix.org"
       ];
 
       trusted-public-keys = [
         "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-        # "rytswd-nix-config.cachix.org-1:fpZQ465aGF2LYQ8oKOrd5c8kxaNmD7wBEK/yyhSQozo="
-        # "ghostty.cachix.org-1:QB389yTa6gTyneehvqG58y0WnHjQOqgnA+wBnpWWxns="
-        # "cosmic.cachix.org-1:Dya9IyXD4xdBehWjrkPv6rtxpmMdRel02smYzA85dPE="
 
         # Unused
         # "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
@@ -69,6 +63,10 @@
         "rytswd-nix-config.cachix.org-1:fpZQ465aGF2LYQ8oKOrd5c8kxaNmD7wBEK/yyhSQozo="
         "ghostty.cachix.org-1:QB389yTa6gTyneehvqG58y0WnHjQOqgnA+wBnpWWxns="
         "cosmic.cachix.org-1:Dya9IyXD4xdBehWjrkPv6rtxpmMdRel02smYzA85dPE="
+
+        "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+        "niri.cachix.org-1:Wv0OmO7PsuocRKzfDoJ3mulSl7Z6oezYhGhR+3W2964="
+        "roc-lang.cachix.org-1:6lZeqLP9SadjmUbskJAvcdGR2T5ViR57pDVkxJQb8R4="
 
         # Unused
         # "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="

643530c82d3face8503a1adb81b7243c9a63761a
Ryota <rytswd@gmail.com>
2025-05-08 04:13:24 +0100

    Add systemd setup for ollama

--- user-config/modules/service/ollama.nix
+++ user-config/modules/service/ollama.nix
@@ -14,6 +14,27 @@
     home.packages = [
       pkgs.ollama-cuda
     ];
-    # TODO: Handle ollama server process
+
+    services.ollama = {
+      enable = true;
+      acceleration = "cuda";
+    };
+
+    # Update the exec to effectively use nvidia-offload.
+    systemd.user.services.ollama = {
+      # NOTE: Due to the laptop sleep handling, I could see a random error of:
+      #
+      #     cuda driver library init failure: 999.
+      #
+      # In case of this error, I just need to run the following command:
+      #
+      #     sudo rmmod nvidia_uvm; sudo modprobe nvidia_uvm
+      environment = {
+        __NV_PRIME_RENDER_OFFLOAD = "1";
+        __NV_PRIME_RENDER_OFFLOAD_PROVIDER = "NVIDIA-G0";
+        __GLX_VENDOR_LIBRARY_NAME = "nvidia";
+        __VK_LAYER_NV_optimus = "NVIDIA_only";
+      };
+    };
   };
 }

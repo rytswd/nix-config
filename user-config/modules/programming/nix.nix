22b5de9877e84ccb56e618726e70a987cad08a3b
Ryota <rytswd@gmail.com>
2025-06-03 02:27:07 +0100

    Add alejandra formatting

--- user-config/modules/programming/nix.nix
+++ user-config/modules/programming/nix.nix
@@ -16,6 +16,7 @@
       pkgs.nvd
       pkgs.nixd # Language Server
       pkgs.nixpkgs-review # PR review utility
+      pkgs.alejandra # Formatting
     ] ++ [
       # Because of Darwin support provided by a separate flake, this ensures
       # there is no conflicting package on macOS. NixOS technically doesn't need

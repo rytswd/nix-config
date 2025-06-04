61443fab31c4a57e6186f36aa475925167c3f6f3
Ryota <rytswd@gmail.com>
2025-06-04 01:51:46 +0100

    Remove localsearch
    I don't need it for my use case with my machines where I just use the
    terminal to search through when I need to. Also, this seems to draw a
    lot of computing power and drains battery.

--- nixos-config/modules/desktop-environment/gnome.nix
+++ nixos-config/modules/desktop-environment/gnome.nix
@@ -21,5 +21,9 @@
       #   sources=[('xkb', 'us+dvorak'), ('xkb', 'us'), ('xkb', 'jp')]
       # '';
     };
+
+    # Remove the local indexing, as this is adding extra load to the machine
+    # unnecessarily, as I don't really use the file indexing.
+    services.gnome.localsearch.enable = false;
   };
 }

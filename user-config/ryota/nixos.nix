49faad06e413ec7f964baa87103836b105b1aaf2
Ryota <rytswd@gmail.com>
2025-06-04 02:06:09 +0100

    Add virtualisation setup

--- user-config/ryota/nixos.nix
+++ user-config/ryota/nixos.nix
@@ -42,6 +42,7 @@ in {
       ../modules/music
       ../modules/video
       ../modules/linux-widget
+      ../modules/virtualisation
 
       # Extra modules based on private setup.
       inputs.nix-config-private.user-modules.email

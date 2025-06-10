5d84f8eba69ca288c3c80f0b2cb28f32bdcf16ee
Ryota <rytswd@gmail.com>
2025-06-10 16:30:20 +0100

    Use limine as the main boot loader

--- nixos-config/modules/core/boot.nix
+++ nixos-config/modules/core/boot.nix
@@ -11,23 +11,42 @@
   config = lib.mkIf config.core.boot.enable {
     boot = {
       loader = {
-        grub.enable = false;
-        # NOTE: I used to have GRUB config at one point, but found it too much
-        # complication for my own needs.
-        # grub = {
-        #   enable = true;
-        #   efiSupport = true;
-        #   device = "nodev";
-        #   fsIdentifier = "label";
-        #   # efiInstallAsRemovable = true;
-        #   gfxmodeEfi = "1920x1200";
-        #   font = "${pkgs.hack-font}/share/fonts/hack/Hack-Regular.ttf";
-        #   fontSize = 36;
-        # };
+        # Only one can be enabled at a time. Others that are not enabled are
+        # there only for reference.
 
-        systemd-boot.enable = true;
-        systemd-boot.editor = true;
-        systemd-boot.consoleMode = "auto";
+        limine = {
+          # Ref: https://github.com/limine-bootloader/limine/blob/v9.x/CONFIG.md
+          enable = true;
+
+          style.wallpapers = [ ./hasliberg.jpg ];
+        };
+
+        grub = {
+          # NOTE: I used to have GRUB config at one point, but found it too much
+          # complication for my own needs.
+          enable = false;
+
+          efiSupport = true;
+          device = "nodev";
+          fsIdentifier = "label";
+          # efiInstallAsRemovable = true;
+          gfxmodeEfi = "1920x1200";
+          font = "${pkgs.hack-font}/share/fonts/hack/Hack-Regular.ttf";
+          fontSize = 36;
+        };
+
+        systemd-boot = {
+          # The systemd-boot seems to be the standard, but I didn't like how it
+          # looked (and the screen resolution was sort of bugging out).
+          # Ref: https://www.freedesktop.org/software/systemd/man/latest/loader.conf.html
+          enable = false;
+
+          editor = true;
+          consoleMode = "auto";
+        };
+
+
+
         efi.canTouchEfiVariables = true;
       };
 

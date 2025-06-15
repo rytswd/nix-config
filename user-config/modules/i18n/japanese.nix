06da21924d17aec4bc42d3d26c359859b616b832
Ryota <rytswd@gmail.com>
2025-06-15 16:30:29 +0100

    Adjust Japanese input setup

--- user-config/modules/i18n/japanese.nix
+++ user-config/modules/i18n/japanese.nix
@@ -1,7 +1,7 @@
 { pkgs
-  , lib
-  , config
-  , ...}:
+, lib
+, config
+, ...}:
 
 {
   options = {
@@ -10,14 +10,66 @@
 
   config = lib.mkIf config.i18n.japanese.enable {
     i18n.inputMethod = {
-      enabled = "fcitx5";
+      type = "fcitx5";
+      enable = true;
       fcitx5 = {
+        waylandFrontend = true;
         addons = with pkgs; [
           fcitx5-configtool
           fcitx5-mozc
           fcitx5-gtk
           fcitx5-catppuccin
         ];
+        settings = {
+          # Corresponds to ~/.config/fcitx5/profile
+          inputMethod = {
+            GroupOrder = {
+              "0" = "Main";
+              "1" = "JP";
+            };
+            "Groups/0" = {
+              Name = "Main";
+              "Default Layout" = "us";
+              DefaultIM = "keyboard-us-dvorak";
+            };
+            "Groups/0/Items/0".Name = "keyboard-us-dvorak";
+
+            "Groups/1" = {
+              Name = "JP";
+              "Default Layout" = "us";
+              DefaultIM = "mozc";
+            };
+            "Groups/1/Items/0".Name = "mozc";
+          };
+
+          # Corresponds to ~/.config/fcitx5/conf/ directory
+          addons = {
+            quickphrase.globalSection = {
+              TriggerKey = ""; # Disable this altogether.
+              "Choose Modifier" = "None";
+              Spell = true;
+              FallbackSpellLanguage = "en";
+            };
+          };
+
+          # Corresponds to ~/.config/fcitx5/config
+          globalOptions = {
+            Behavior = {
+              ActiveByDefault = false;
+            };
+            "Hotkey/TriggerKeys" = {
+              "0" = "F8";
+            };
+            "Hotkey/EnumerateGroupForwardKeys" = {
+              "0" = "F9";
+            };
+            Hotkey = {
+              EnumerateWithTriggerKeys = true;
+              EnumerateSkipFirst = false;
+              ModifierOnlyKeyTimeout = 250;
+            };
+          };
+        };
       };
     };
   };

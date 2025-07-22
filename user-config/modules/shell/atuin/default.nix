50f2a6a22bc1009b0acf1bcd9697da9a94105252
Ryota <rytswd@gmail.com>
2025-07-22 02:34:55 +0100

    Adjust atuin config with SOPS Nix
    Now this contains my Atuin server which is only wired up using SOPS Nix.

--- user-config/modules/shell/atuin/default.nix
+++ user-config/modules/shell/atuin/default.nix
@@ -8,28 +8,57 @@
     shell.atuin.enable = lib.mkEnableOption "Enable Atuin.";
   };
 
-  config = lib.mkIf config.shell.atuin.enable {
-    programs.atuin = {
-      enable = true;
-      enableBashIntegration = true;
-      enableZshIntegration = true;
-      enableFishIntegration = true;
-      enableNushellIntegration = true;
+  config = let
+    # Base setup
+    base = lib.mkIf config.shell.atuin.enable {
+      programs.atuin = {
+        enable = true;
+        enableBashIntegration = true;
+        enableZshIntegration = true;
+        enableFishIntegration = true;
+        enableNushellIntegration = true;
 
-      flags = [
-        # The up arrow binding gets triggered by mistake, and while I tried to
-        # adopt for some time, I found it more annoying than useful. When I
-        # need to check the actual history, I will be sending an explicit keys
-        # and that would give me better control.
-        "--disable-up-arrow"
-      ];
-      settings = {
-        auto_sync = true;
-        sync_frequency = "5m";
-        search_mode = "fuzzy";
-        filter_mode_shell_up_key_binding = "session";
-        enter_accept = false; # Do not immediately execute
+        flags = [
+          # The up arrow binding gets triggered by mistake, and while I tried to
+          # adopt for some time, I found it more annoying than useful. When I
+          # need to check the actual history, I will be sending an explicit keys
+          # and that would give me better control.
+          "--disable-up-arrow"
+        ];
+
+        # NOTE: Because of the lack of secret handling, I'm using XDG file with
+        # SOPS Nix.
+        # settings = {
+        #   auto_sync = true;
+        #   sync_frequency = "5m";
+        #   sync_address = "REDACTED;
+        #   search_mode = "fuzzy";
+        #   filter_mode_shell_up_key_binding = "session";
+        #   enter_accept = false; # Do not immediately execute
+        # };
       };
     };
-  };
+
+    # SOPS Nix based secret handling
+    withSops = lib.mkIf config.security.sops-nix.enable {
+      # Use sops.templates to generate the config with secrets substituted.
+      sops.templates."atuin-config" = {
+        # With file input like this, the file is expected to have the following
+        # placeholder string:
+        #
+        #     <SOPS:**SHA256_OF_SECRET**:PLACEHOLDER>
+        #
+        # And SHA256 can be generated using
+        #
+        #     nix repl
+        #     > builtins.hashString "sha256" "SECRET_NAME"
+        #
+        file = ./config.toml;
+        path = "${config.xdg.configHome}/atuin/config.toml";
+      };
+    };
+  in lib.mkMerge [
+    base
+    withSops
+  ];
 }

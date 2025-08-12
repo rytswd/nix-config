e4bb03f037958d24e558d91243ea9d018430a8f2
Ryota <rytswd@gmail.com>
2025-08-12 00:53:54 -0400

    Correct swayidle and lock logic

--- user-config/modules/session-lock/swayidle.nix
+++ user-config/modules/session-lock/swayidle.nix
@@ -13,15 +13,38 @@
       enable = true;
       # systemdTarget = "sway-session.target";
       events = [
-        { event = "before-sleep"; command = "${pkgs.swaylock-effects}/bin/swaylock"; }
+        {
+          # This is needed when suspend is called by other logic, such as lid close.
+          event = "before-sleep";
+          command = "${pkgs.swaylock-effects}/bin/swaylock -fF";
+        }
       ];
       timeouts = [
+        # 10 min
+        {
+          timeout = (60 * 10);
+          # TODO: This is specific to the Asus machine, I will need to move this
+          # definition somewhere else.
+          command = "${pkgs.brightnessctl}/bin/brightnessctl -d \"amdgpu_bl*\" set 5% -s";
+          resumeCommand = "${pkgs.brightnessctl}/bin/brightnessctl -d \"amdgpu_bl*\" -r";
+        }
         # 15 min
-        { timeout = (60 * 15); command = "${pkgs.swaylock-effects}/bin/swaylock"; }
-        # 30 min
-        { timeout = (60 * 30); command = "${pkgs.systemd}/bin/systemctl suspend"; }
+        {
+          timeout = (60 * 15);
+          # NOTE: The swaylock -f flag is necessary to free up swayidle to
+          # process further suspention.
+          command = "${pkgs.swaylock-effects}/bin/swaylock -fF";
+        }
+        # 20 min
+        {
+          timeout = (60 * 20);
+          command = "${pkgs.systemd}/bin/systemctl suspend";
+        }
         # TODO: Hibernate setup needs more tweaking, and thus commenting out for now.
-        # { timeout = (60 * 30); command = "${pkgs.systemd}/bin/systemctl suspend-then-hibernate"; }
+        # {
+        #   timeout = (60 * 30);
+        #   command = "${pkgs.systemd}/bin/systemctl suspend-then-hibernate";
+        # }
       ];
     };
 

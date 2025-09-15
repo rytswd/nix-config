a92a6519094bcc6d5658a8dde990aec6c4a4a7ff
Ryota <rytswd@gmail.com>
2025-09-15 22:56:00 +0100

    Correct Ollama setup and use default

--- user-config/modules/service/ollama.nix
+++ user-config/modules/service/ollama.nix
@@ -15,29 +15,43 @@
       pkgs.ollama-cuda
     ];
 
+    # NOTE: Due to the laptop sleep handling, I could see a random error of:
+    #
+    #     cuda driver library init failure: 999.
+    #
+    # In case of this error, I just need to run the following command:
+    #
+    #     sudo rmmod nvidia_uvm; sudo modprobe nvidia_uvm
+    #
+    # However, this assumes there is no other GPU usage, or if they are, they
+    # could be safely killed with the above command.
+
+    # NOTE: Because of the nvidia-offload setup, the setup below needed tweak.
+    # Following some recent changes, it seems unnecessary, and most parts are
+    # commented out for now.
+
     services.ollama = {
       enable = true;
+      package = pkgs.ollama-cuda;
       acceleration = "cuda";
+      # environmentVariables = {
+      #   # NOTE: The below env mimics the use of nvidia-offload.
+      #   __NV_PRIME_RENDER_OFFLOAD = "1";
+      #   __NV_PRIME_RENDER_OFFLOAD_PROVIDER = "NVIDIA-G0";
+      #   __GLX_VENDOR_LIBRARY_NAME = "nvidia";
+      #   __VK_LAYER_NV_optimus = "NVIDIA_only";
+      # };
     };
 
-    # Update the exec to effectively use nvidia-offload.
-    systemd.user.services.ollama = {
-      # NOTE: Due to the laptop sleep handling, I could see a random error of:
-      #
-      #     cuda driver library init failure: 999.
-      #
-      # In case of this error, I just need to run the following command:
-      #
-      #     sudo rmmod nvidia_uvm; sudo modprobe nvidia_uvm
-      #
-      # However, this assumes there is no other GPU usage, or if they are, they
-      # could be safely killed with the above command.
-      environment = {
-        __NV_PRIME_RENDER_OFFLOAD = "1";
-        __NV_PRIME_RENDER_OFFLOAD_PROVIDER = "NVIDIA-G0";
-        __GLX_VENDOR_LIBRARY_NAME = "nvidia";
-        __VK_LAYER_NV_optimus = "NVIDIA_only";
-      };
-    };
+    # # Update the exec to effectively use nvidia-offload.
+    # systemd.user.services.ollama = {
+    #   # NOTE: The below env mimics the use of nvidia-offload.
+    #   environment = {
+    #     __NV_PRIME_RENDER_OFFLOAD = "1";
+    #     __NV_PRIME_RENDER_OFFLOAD_PROVIDER = "NVIDIA-G0";
+    #     __GLX_VENDOR_LIBRARY_NAME = "nvidia";
+    #     __VK_LAYER_NV_optimus = "NVIDIA_only";
+    #   };
+    # };
   };
 }

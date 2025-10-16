cc913e16234a55e2b75bf0b798f0ab5f05d2870d
Ryota <rytswd@gmail.com>
2025-10-17 00:20:43 +0100

    Update Claude Code version

--- overlays/claude-code.nix
+++ overlays/claude-code.nix
@@ -3,10 +3,10 @@ let
   # NOTE: For updating the version, run the following:
   #
   # nix shell nixpkgs#nix-prefetch
-  # nix-prefetch fetchzip --url "https://registry.npmjs.org/@anthropic-ai/claude-code/-/claude-code-2.0.0.tgz"
+  # nix-prefetch fetchzip --url "https://registry.npmjs.org/@anthropic-ai/claude-code/-/claude-code-2.0.14.tgz"
   # npm hash only changes when the dependency updates, and only found after built.
-  claude-code-version = "2.0.0";
-  claude-code-src-hash = "sha256-uHU9SZso0OZkbcroaVqqVoDvpn28rZVc6drHBrElt5M=";
+  claude-code-version = "2.0.14";
+  claude-code-src-hash = "sha256-U/wd00eva/qbSS4LpW1L7nmPW4dT9naffeMkHQ5xr5o=";
   claude-code-npmDepsHash = "sha256-LkQf2lW6TM1zRr10H7JgtnE+dy0CE7WCxF4GhTd4GT4=";
 in
 {

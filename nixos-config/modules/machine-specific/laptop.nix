11104fffa800a843912b5fd8bbd4c58bb308d5ac
Ryota <rytswd@gmail.com>
2025-10-24 01:06:37 +0100

    Add power handling

--- nixos-config/modules/machine-specific/laptop.nix
+++ nixos-config/modules/machine-specific/laptop.nix
@@ -10,4 +10,29 @@
   systemd.sleep.extraConfig = ''
     HibernateDelaySec=1h
   '';
+
+  # Ref: https://discourse.nixos.org/t/battery-life-still-isnt-great/41188/3
+  # TODO: I need to review this more closely, it's working OK but could be better.
+  powerManagement = {
+    enable = true;
+    powertop.enable = true;
+    cpuFreqGovernor = "powersave";
+  };
+  services = {
+    thermald.enable = true;
+    power-profiles-daemon.enable = false;
+    auto-cpufreq = {
+      enable = true;
+      settings = {
+        battery = {
+          governor = "powersave";
+          turbo = "never";
+        };
+        charger = {
+          governor = "powersave";
+          turbo = "auto";
+        };
+      };
+    };
+  };
 }

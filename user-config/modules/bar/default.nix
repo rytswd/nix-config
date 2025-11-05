f51a9899f26bb47b490d540bcaac5d9480183483
Ryota <rytswd@gmail.com>
2025-11-05 11:11:52 +0000

    Remove ags usage

--- user-config/modules/bar/default.nix
+++ user-config/modules/bar/default.nix
@@ -6,13 +6,11 @@
 {
   imports = [
     ./waybar
-    # ./waybar.nix # NOTE: This is the old one based on Nix config.
-    ./ags
+    # ./ags
     ./sketchybar
   ];
 
   # NOTE: I'm setting no default here, so importing this bar module does not
   # enable any bar by default. There could be an OS based check I could add
   # to the mix, but for now, this is good enough.
-  # bar.waybar.enable = lib.mkDefault true;
 }

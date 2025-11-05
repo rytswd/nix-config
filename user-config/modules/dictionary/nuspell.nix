5de7ca4528fed98fcf100b9b9f543af067792608
Ryota <rytswd@gmail.com>
2025-11-05 11:14:57 +0000

    Correct nushell spec

--- user-config/modules/dictionary/nuspell.nix
+++ user-config/modules/dictionary/nuspell.nix
@@ -12,10 +12,10 @@
     home.packages = [
       # https://github.com/nuspell/nuspell
       # NOTE: Nuspell uses the same dictionary as Hunspell.
-      (pkgs.nuspellWithDicts [
+      (pkgs.nuspell.withDicts (_: [
         pkgs.hunspellDicts.en_GB-large
         pkgs.hunspellDicts.en_US-large
-      ])
+      ]))
     ];
   };
 }

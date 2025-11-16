8467f695ec2550afed6d7ccab3503301dcb4b545
Ryota <rytswd@gmail.com>
2025-11-16 23:05:48 +0000

    Add anyrun for testing

---
+++ user-config/modules/launcher/anyrun.nix
@@ -0,0 +1,50 @@
+{ pkgs
+, lib
+, config
+, inputs
+, ...}:
+
+{
+  options = {
+    launcher.anyrun.enable = lib.mkEnableOption "Enable Anyrun.";
+  };
+
+  config = lib.mkIf config.launcher.anyrun.enable {
+    programs.anyrun = {
+      enable = true;
+      config = {
+        x = { fraction = 0.5; };
+        y = { fraction = 0.3; };
+        width = { fraction = 0.3; };
+        hideIcons = false;
+        ignoreExclusiveZones = false;
+        layer = "overlay";
+        hidePluginInfo = false;
+        closeOnClick = false;
+        showResultsImmediately = false;
+        maxEntries = null;
+
+        plugins = [
+          "${pkgs.anyrun}/lib/libapplications.so"
+          "${pkgs.anyrun}/lib/libsymbols.so"
+        ];
+      };
+
+      # Inline comments are supported for language injection into
+      # multi-line strings with Treesitter! (Depends on your editor)
+      extraCss = /*css */ ''
+        .some_class {
+          background: red;
+        }
+      '';
+
+      extraConfigFiles."some-plugin.ron".text = ''
+        Config(
+        // for any other plugin
+        // this file will be put in ~/.config/anyrun/some-plugin.ron
+        // refer to docs of xdg.configFile for available options
+      )
+      '';
+    };
+  };
+}

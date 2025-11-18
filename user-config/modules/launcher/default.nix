2564205ac983ea98d6c3efb0ee67af0e6ca8c27c
Ryota <rytswd@gmail.com>
2025-11-18 16:32:05 +0000

    Add walker for reference
    NOTE: The configuration for walker is not managed within Nix setup at
    the moment, and without the config directory, it fails to do
    anything. This was handled manually for now, but I may decide to
    simply drop walker if I don't see it fit.

--- user-config/modules/launcher/default.nix
+++ user-config/modules/launcher/default.nix
@@ -8,11 +8,13 @@
     ./anyrun.nix
     ./fuzzel.nix
     ./rofi.nix
+    ./walker.nix
     ./wofi.nix
   ];
 
   launcher.anyrun.enable = lib.mkDefault true;
   launcher.fuzzel.enable = lib.mkDefault true;
   launcher.rofi.enable = lib.mkDefault true;
+  launcher.walker.enable = lib.mkDefault true;
   launcher.wofi.enable = lib.mkDefault false; # Being explicit
 }

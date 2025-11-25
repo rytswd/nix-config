e067e60e0ddb8676b81a2b203371f729cd8d1573
Ryota <rytswd@gmail.com>
2025-11-25 01:12:12 +0000

    Adjust GTK settings for both GTK3 and GTK4

--- user-config/modules/appearance/gtk.nix
+++ user-config/modules/appearance/gtk.nix
@@ -26,29 +26,34 @@
         name = "Bibata-Modern-Ice";
       };
 
-      gtk3.extraConfig = {
-        gtk-application-prefer-dark-theme = true;
-        # NOTE: This sets the Emacs based key bindings such as C-a to go to the
-        # beginning of line.
-        gtk-key-theme-name = "Emacs";
-        # TODO: This is commented out for now.
-        # gtk-im-module = "fcitx";
+      gtk3 = {
+        extraConfig = {
+          gtk-application-prefer-dark-theme = true;
+          # NOTE: This sets the Emacs based key bindings such as C-a to go to the
+          # beginning of line.
+          gtk-key-theme-name = "Emacs";
+          # TODO: This is commented out for now.
+          # gtk-im-module = "fcitx";
+        };
+        # extraCss = ''
+        #   @binding-set MyCustomBindings {
+        #    bind "<Meta>j" { "copy-clipboard" () };
+        #   }
+
+        #   * {
+        #     gtk-key-bindings: MyCustomBindings;
+        #   }
+        # '';
       };
 
-      # gtk3.extraCss = ''
-      #   @binding-set MyCustomBindings {
-      #    bind "<Meta>j" { "copy-clipboard" () };
-      #   }
 
-      #   * {
-      #     gtk-key-bindings: MyCustomBindings;
-      #   }
-      # '';
-
-      # NOTE: GTK4 commented out for now.
-      # gtk4.extraConfig = {
-      #   gtk-application-prefer-dark-theme = true;
-      # };
+      gtk4 = {
+        # colorScheme = "dark";
+        extraConfig = {
+          gtk-interface-color-scheme = "dark";
+          gtk-application-prefer-dark-theme = true;
+        };
+      };
     };
   };
 }

dc01ffb81065674c7e823fef75c27d3efeba747a
Ryota <rytswd@gmail.com>
2025-12-24 01:51:29 -0500

    Reformat and add impermanence config

--- user-config/admin/create.nix
+++ user-config/admin/create.nix
@@ -1,29 +1,47 @@
-{ pkgs
-, ... }:
+{ pkgs, lib, config, ... }:
 
-let admin =
-  if pkgs.stdenv.isDarwin then {
-    home = "/Users/admin";
-  } else {
-    home = "/home/admin";
-    group = "wheel";
+let
+  admin =
+    if pkgs.stdenv.isDarwin then
+      {
+        home = "/Users/admin";
+      }
+    else
+      {
+        home = "/home/admin";
+        group = "wheel";
 
-    # For running scripts, zsh is easier as it works similarly to bash.
-    shell = pkgs.bash;
+        # For running scripts, zsh is easier as it works similarly to bash.
+        shell = pkgs.bash;
 
-    # IMPORTANT: The admin user is not a normal user.
-    isSystemUser = true;
+        # IMPORTANT: The admin user is not a normal user.
+        isSystemUser = true;
 
-    extraGroups = [
-      "input" # For Xremap and input handling without sudo
-      "uinput" # For Xremap and input handling without sudo
-    ];
+        extraGroups = [
+          "input"  # For Xremap and input handling without sudo
+          "uinput" # For Xremap and input handling without sudo
+        ];
 
-    # Set initial password.
-    # When the `users.mutableUsers` is set, this would only apply for the new
-    # user creation. User can later change the password using `passwd` command.
-    initialHashedPassword = "$6$hGTdy9p9p203$8oeOAgXLzkKdo5HUkydZkEYQbWxzXgtjMsmSB76PkO6p/JWbJuJ9FhMXmhibm.XqZD58pR8hlc5EocdncS72s/"; # root
+        # Set initial password.
+        # When the `users.mutableUsers` is set, this would only apply for the new
+        # user creation. User can later change the password using `passwd` command.
+        initialHashedPassword = "$6$hGTdy9p9p203$8oeOAgXLzkKdo5HUkydZkEYQbWxzXgtjMsmSB76PkO6p/JWbJuJ9FhMXmhibm.XqZD58pR8hlc5EocdncS72s/"; # root
+      };
+in
+{
+  users.users.admin = admin;
+
+  # When using impermanence, configure what directories get persisted.
+  config = lib.mkIf config.core.boot.impermanence.enable {
+    environment.persistence."/nix/persist" = {
+      users.admin = {
+        directories = [
+          ".ssh"
+        ];
+        files = [
+          # To be updated
+        ];
+      };
+    };
   };
-  in {
-    users.users.admin = admin;
-  }
+}
--- user-config/ryota/create.nix
+++ user-config/ryota/create.nix
@@ -1,29 +1,121 @@
-{ pkgs
-, ... }:
+{ pkgs, lib, config, ... }:
 
-let ryota =
-  if pkgs.stdenv.isDarwin then {
-    home = "/Users/ryota";
-  } else {
-    home = "/home/ryota";
+let
+  ryota =
+    if pkgs.stdenv.isDarwin then
+      {
+        home = "/Users/ryota";
+      }
+    else
+      {
+        home = "/home/ryota";
 
-    shell = pkgs.fish;
+        shell = pkgs.fish;
 
-    isNormalUser = true;
-    extraGroups = [
-      "wheel" # For sudo
-      "input" # For Xremap and input handling without sudo
-      "uinput" # For Xremap and input handling without sudo
-      "networkmanager"# For nmcli etc.
-      "docker" # For docker
-      "libvirtd" # For VM
-    ];
+        isNormalUser = true;
+        extraGroups = [
+          "wheel"          # For sudo
+          "input"          # For Xremap and input handling without sudo
+          "uinput"         # For Xremap and input handling without sudo
+          "networkmanager" # For nmcli etc.
+          "docker"         # For docker
+          "libvirtd"       # For VM
+        ];
 
-    # Set initial password.
-    # When the `users.mutableUsers` is set, this would only apply for the new
-    # user creation. User can later change the password using `passwd` command.
-    initialHashedPassword = "$6$hGTdy9p9p203$8oeOAgXLzkKdo5HUkydZkEYQbWxzXgtjMsmSB76PkO6p/JWbJuJ9FhMXmhibm.XqZD58pR8hlc5EocdncS72s/"; # root
+        # Set initial password.
+        # When the `users.mutableUsers` is set, this would only apply for the new
+        # user creation. User can later change the password using `passwd` command.
+        initialHashedPassword = "$6$hGTdy9p9p203$8oeOAgXLzkKdo5HUkydZkEYQbWxzXgtjMsmSB76PkO6p/JWbJuJ9FhMXmhibm.XqZD58pR8hlc5EocdncS72s/"; # root
+      };
+in
+{
+  users.users.ryota = ryota;
+
+  # When using impermanence, configure what directories get persisted.
+  config = lib.mkIf config.core.boot.impermanence.enable {
+    environment.persistence."/nix/persist" = {
+      users.ryota = {
+        directories = [
+          ###----------------------------------------
+          ##  Common directories
+          #------------------------------------------
+          "Coding"
+          "Documents"
+          "Downloads"
+          "Email"
+          "Music"
+          "Pictures"
+          "Videos"
+
+          ###----------------------------------------
+          ##  dotfiles
+          #------------------------------------------
+          ".password-store"
+          ".emacs.d"
+          ".ssh"
+          ".gnupg"
+
+          ###========================================
+          ##  XDG Cache & Data & State
+          #==========================================
+          # Key files I should always need
+          ".cache/fontconfig"
+          ".cache/nix"
+          ".local/share/direnv"
+          ".local/share/fonts"
+
+          ###----------------------------------------
+          ##  Application specific
+          #------------------------------------------
+          # Emacs
+          ".cache/mu"
+          ".cache/emacs"
+          ".cache/org-persist"
+          ".local/share/emacs"
+          ".local/state/emacs"
+
+          # Proton
+          ".cache/Proton"
+
+          # Browsers
+          ".cache/vivaldi"
+          ".cache/zen"
+
+          # Steam
+          ".local/share/Steam"
+          ".steam"
+
+          ###----------------------------------------
+          ##  Tooling
+          #------------------------------------------
+          ".cache/awww"
+          ".local/share/atuin"
+          ".local/share/docker"
+          ".local/share/zoxide"
+          ".local/share/kubebuilder-envtest"
+
+          ###----------------------------------------
+          ##  Coding
+          #------------------------------------------
+          # Zig
+          ".cache/zig"
+          ".cache/zls"
+
+          # Go
+          ".cache/golangci-lint"
+          ".cache/gopls"
+
+          # JavaScript
+          ".cache/bun"
+          ".cache/pnpm"
+          ".cache/mesa_shader_cache"
+          ".cache/ms-playwright"
+          ".cache/ms-playwright-go"
+        ];
+        files = [
+          # To be updated
+        ];
+      };
+    };
   };
-  in {
-    users.users.ryota = ryota;
-  }
+}

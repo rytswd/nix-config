01e5cca14d9b08cc083b241ebd9f8f5869f94a29
Ryota <rytswd@gmail.com>
2025-12-24 01:50:52 -0500

    Add impermanence

--- nixos-config/modules/core/boot/default.nix
+++ nixos-config/modules/core/boot/default.nix
@@ -5,14 +5,19 @@
 
 {
   imports = [
-    ./common.nix
+    ./common.nix # No flag to enable / disable
+
     ./grub.nix
     ./limine.nix
     ./systemd-boot.nix
+    ./impermanence.nix
   ];
 
   # Explicitly disabling all. Only one can be enabled at a time.
   core.boot.limine.enable = lib.mkDefault false;
   core.boot.grub.enable = lib.mkDefault false;
   core.boot.systemd-boot.enable = lib.mkDefault false;
+
+  # Impermanence requires systemd-boot.
+  core.boot.impermanence.enable = lib.mkDefault false;
 }
---
+++ nixos-config/modules/core/boot/impermanence.nix
@@ -0,0 +1,77 @@
+{
+  pkgs,
+  lib,
+  config,
+  ...
+}:
+
+{
+  options = {
+    core.boot.impermanence.enable = lib.mkEnableOption "Enable impermanence.";
+  };
+
+  config = lib.mkIf config.core.boot.impermanence.enable {
+    # Only added to force build failure when missing.
+    boot.loader.systemd-boot.enable = true;
+
+    fileSystems."/nix/persist".neededForBoot = true;
+
+    # TODO: Review this to make it work, currently failing to start up.
+    # boot.initrd.systemd = {
+    #   enable = true;
+    #   rollback = {
+    #     description = "Rollback ZFS root to blank snapshot";
+    #     wantedBy = [ "initrd.target" ];
+    #     after = [ "zfs-import-zroot.service" ];
+    #     before = [ "sysroot.mount" ];
+    #     path = with pkgs; [ zfs ];
+    #     unitConfig.DefaultDependencies = "no";
+    #     serviceConfig.Type = "oneshot";
+    #     script = ''
+    #       # 1. Import the pool (read-only likely, so we might need to be careful)
+    #       # In systemd initrd, zfs-import handles the import.
+
+    #       # 2. Roll the old root (Keep 10 generations, for example)
+    #       # We rename the current 'root' to 'root-timestamp'
+
+    #       dataset="zroot/store/root"
+    #       timestamp=$(date +%Y%m%d%H%M%S)
+
+    #       # Move the current state to a backup
+    #       # Note: We rollback to @blank immediately after, creating a fresh slate.
+    #       # If you strictly want 'rolling' backups accessible:
+    #       zfs rename "$dataset" "$dataset-old-$timestamp"
+
+    #       # clone the blank snapshot to create the new active root
+    #       zfs send "zroot/store/root@blank" | zfs receive "$dataset"
+
+    #       # Optional: Clean up very old datasets here if desired
+    #     '';
+    #   };
+    # };
+
+    # Everything in / is wiped on boot. I need to define what survives here.
+    # For user home directory, they are configured separately per user.
+    environment.persistence."/nix/persist" = {
+      hideMounts = true;
+      directories = [
+        "/var/log"
+        "/var/lib/nixos"
+        "/var/lib/bluetooth"
+        "/var/lib/containers"
+        "/var/lib/fwupd"
+        "/var/lib/iwd"
+        "/var/lib/syncthing"
+        "/var/lib/systemd/coredump"
+        "/etc/NetworkManager/system-connections"
+      ];
+      files = [
+        "/etc/machine-id"
+        "/etc/ssh/ssh_host_ed25519_key"
+        "/etc/ssh/ssh_host_ed25519_key.pub"
+        "/etc/ssh/ssh_host_rsa_key"
+        "/etc/ssh/ssh_host_rsa_key.pub"
+      ];
+    };
+  };
+}

cd6e49c4d5fd030625ef0f08ed89211532ae86b9
Ryota <rytswd@gmail.com>
2025-12-24 23:23:00 -0500

    Use home-manager impermanence
    Note that this would actually break the standalone home-manager. For
    that reason, this is not meant to be used with the home-manager
    configuration, but instead pulled in with nixos setup.

--- user-config/admin/persist-impermanence.nix
+++ user-config/admin/persist-impermanence.nix
@@ -1,14 +1,13 @@
 # Impermanence based persisting file setting for files and directories.
 # Only import when impermanence is used.
 {
-  environment.persistence."/nix/persist" = {
-    users.admin = {
-      directories = [
-        ".ssh"
-      ];
-      files = [
-        # To be updated
-      ];
-    };
+  home.persistence."/nix/persist" = {
+    allowOther = true;
+    directories = [
+      ".ssh"
+    ];
+    files = [
+      # To be updated
+    ];
   };
 }
--- user-config/ryota/persist-impermanence.nix
+++ user-config/ryota/persist-impermanence.nix
@@ -1,88 +1,87 @@
 # Impermanence based persisting file setting for files and directories.
 # Only import when impermanence is used.
 {
-  environment.persistence."/nix/persist" = {
-    users.ryota = {
-      directories = [
-        ###----------------------------------------
-        ##  Common directories
-        #------------------------------------------
-        "Coding"
-        "Documents"
-        "Downloads"
-        "Email"
-        "Music"
-        "Pictures"
-        "Videos"
+  home.persistence."/nix/persist" = {
+    allowOther = true;
+    directories = [
+      ###----------------------------------------
+      ##  Common directories
+      #------------------------------------------
+      "Coding"
+      "Documents"
+      "Downloads"
+      "Email"
+      "Music"
+      "Pictures"
+      "Videos"
 
-        ###----------------------------------------
-        ##  dotfiles
-        #------------------------------------------
-        ".password-store"
-        ".emacs.d"
-        ".ssh"
-        ".gnupg"
+      ###----------------------------------------
+      ##  dotfiles
+      #------------------------------------------
+      ".password-store"
+      ".emacs.d"
+      ".ssh"
+      ".gnupg"
 
-        ###========================================
-        ##  XDG Cache & Data & State
-        #==========================================
-        # Key files I should always need
-        ".cache/fontconfig"
-        ".cache/nix"
-        ".local/share/direnv"
-        ".local/share/fonts"
+      ###========================================
+      ##  XDG Cache & Data & State
+      #==========================================
+      # Key files I should always need
+      ".cache/fontconfig"
+      ".cache/nix"
+      ".local/share/direnv"
+      ".local/share/fonts"
 
-        ###----------------------------------------
-        ##  Application specific
-        #------------------------------------------
-        # Emacs
-        ".cache/mu"
-        ".cache/emacs"
-        ".cache/org-persist"
-        ".local/share/emacs"
-        ".local/state/emacs"
+      ###----------------------------------------
+      ##  Application specific
+      #------------------------------------------
+      # Emacs
+      ".cache/mu"
+      ".cache/emacs"
+      ".cache/org-persist"
+      ".local/share/emacs"
+      ".local/state/emacs"
 
-        # Proton
-        ".cache/Proton"
+      # Proton
+      ".cache/Proton"
 
-        # Browsers
-        ".cache/vivaldi"
-        ".cache/zen"
+      # Browsers
+      ".cache/vivaldi"
+      ".cache/zen"
 
-        # Steam
-        ".local/share/Steam"
-        ".steam"
+      # Steam
+      ".local/share/Steam"
+      ".steam"
 
-        ###----------------------------------------
-        ##  Tooling
-        #------------------------------------------
-        ".cache/awww"
-        ".local/share/atuin"
-        ".local/share/docker"
-        ".local/share/zoxide"
-        ".local/share/kubebuilder-envtest"
+      ###----------------------------------------
+      ##  Tooling
+      #------------------------------------------
+      ".cache/awww"
+      ".local/share/atuin"
+      ".local/share/docker"
+      ".local/share/zoxide"
+      ".local/share/kubebuilder-envtest"
 
-        ###----------------------------------------
-        ##  Coding
-        #------------------------------------------
-        # Zig
-        ".cache/zig"
-        ".cache/zls"
+      ###----------------------------------------
+      ##  Coding
+      #------------------------------------------
+      # Zig
+      ".cache/zig"
+      ".cache/zls"
 
-        # Go
-        ".cache/golangci-lint"
-        ".cache/gopls"
+      # Go
+      ".cache/golangci-lint"
+      ".cache/gopls"
 
-        # JavaScript
-        ".cache/bun"
-        ".cache/pnpm"
-        ".cache/mesa_shader_cache"
-        ".cache/ms-playwright"
-        ".cache/ms-playwright-go"
-      ];
-      files = [
-        # To be updated
-      ];
-    };
+      # JavaScript
+      ".cache/bun"
+      ".cache/pnpm"
+      ".cache/mesa_shader_cache"
+      ".cache/ms-playwright"
+      ".cache/ms-playwright-go"
+    ];
+    files = [
+      # To be updated
+    ];
   };
 }

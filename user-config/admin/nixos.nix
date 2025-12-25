04ea94f2ae8468714def3a9710f587528aa86e42
Ryota <rytswd@gmail.com>
2025-12-24 23:46:50 -0500

    Format user config

--- user-config/admin/nixos.nix
+++ user-config/admin/nixos.nix
@@ -4,66 +4,70 @@
 # limited amount of programs and settings are pulled in.
 #
 
-{ config
-, pkgs
-, system
-, inputs
-, ... }:
+{
+  config,
+  pkgs,
+  system,
+  inputs,
+  ...
+}:
 
-let username = "admin";
-in {
-    imports = [
-      # The shell setup defines some aliases, and in order to allow overriding,
-      # calling this earlier than other modules.
-      # The rest of the module call order is rather arbitrary, just in order of
-      # importance for my own use case.
-      ../modules/shell
-      ../modules/key-remap/xremap
-      ../modules/appearance
-      ../modules/window-manager
-      ../modules/launcher
-      ../modules/bar
-      ../modules/clipboard
-      ../modules/notification
-      ../modules/terminal
-      ../modules/vcs
-      ../modules/wallpaper
-      ../modules/session-lock
-      ../modules/browser
-      ../modules/editor
-      ../modules/vpn
-      ../modules/security
-      ../modules/linux-widget
+let
+  username = "admin";
+in
+{
+  imports = [
+    # The shell setup defines some aliases, and in order to allow overriding,
+    # calling this earlier than other modules.
+    # The rest of the module call order is rather arbitrary, just in order of
+    # importance for my own use case.
+    ../modules/shell
+    ../modules/key-remap/xremap
+    ../modules/appearance
+    ../modules/window-manager
+    ../modules/launcher
+    ../modules/bar
+    ../modules/clipboard
+    ../modules/notification
+    ../modules/terminal
+    ../modules/vcs
+    ../modules/wallpaper
+    ../modules/session-lock
+    ../modules/browser
+    ../modules/editor
+    ../modules/vpn
+    ../modules/security
+    ../modules/linux-widget
 
-      # Add Nix related toolings only
-      ../modules/programming/nix.nix
+    # Add Nix related toolings only
+    ../modules/programming/nix.nix
+  ];
+
+  ###----------------------------------------
+  ##   Module related options
+  #------------------------------------------
+  bar.waybar.enable = true;
+  editor.helix.enable = false;
+  editor.vscode.enable = false;
+  notification.swaync.enable = true;
+
+  programming.nix.enable = true;
+
+  ###----------------------------------------
+  ##   Other Home Manager Setup
+  #------------------------------------------
+  programs.home-manager.enable = true;
+  xdg.enable = true;
+
+  home = {
+    username = "${username}";
+    homeDirectory = "/home/${username}";
+
+    packages = [
+      # Utility
+      pkgs.mesa-demos # For OpenGL etc.
     ];
 
-    ###----------------------------------------
-    ##   Module related options
-    #------------------------------------------
-    bar.waybar.enable = true;
-    editor.helix.enable = false;
-    editor.vscode.enable = false;
-    notification.swaync.enable = true;
-
-    programming.nix.enable = true;
-
-    ###----------------------------------------
-    ##   Other Home Manager Setup
-    #------------------------------------------
-    programs.home-manager.enable = true;
-    xdg.enable = true;
-
-    home = {
-      username = "${username}";
-      homeDirectory = "/home/${username}";
-
-      packages = [
-        # Utility
-        pkgs.mesa-demos # For OpenGL etc.
-      ];
-
-      stateVersion = "24.05";
-    };
-  }
+    stateVersion = "24.05";
+  };
+}
--- user-config/ryota/nixos.nix
+++ user-config/ryota/nixos.nix
@@ -1,84 +1,88 @@
 # NixOS specific Home Manager configurations
 
-{ config
-, pkgs
-, system
-, inputs
-, ... }:
+{
+  config,
+  pkgs,
+  system,
+  inputs,
+  ...
+}:
 
-let username = "ryota";
-in {
-    imports = [
-      # The shell setup defines some aliases, and in order to allow overriding,
-      # calling this earlier than other modules.
-      ../modules/shell
-      # The rest of the module call order is rather arbitrary, just in order of
-      # importance for my own use case.
-      ../modules/key-remap/xremap
-      ../modules/appearance
-      ../modules/window-manager
-      ../modules/launcher
-      ../modules/bar
-      ../modules/clipboard
-      ../modules/notification
-      ../modules/process
-      ../modules/terminal
-      ../modules/vcs
-      ../modules/wallpaper
-      ../modules/session-lock
-      ../modules/browser
-      ../modules/editor
-      ../modules/programming
-      ../modules/vpn
-      ../modules/security
-      ../modules/kubernetes
-      ../modules/service
-      ../modules/file-management
-      ../modules/dictionary
-      ../modules/communication
-      ../modules/i18n
-      ../modules/image
-      ../modules/screenshot
-      ../modules/music
-      ../modules/video
-      ../modules/linux-widget
-      ../modules/llm
-      ../modules/virtualisation
+let
+  username = "ryota";
+in
+{
+  imports = [
+    # The shell setup defines some aliases, and in order to allow overriding,
+    # calling this earlier than other modules.
+    ../modules/shell
+    # The rest of the module call order is rather arbitrary, just in order of
+    # importance for my own use case.
+    ../modules/key-remap/xremap
+    ../modules/appearance
+    ../modules/window-manager
+    ../modules/launcher
+    ../modules/bar
+    ../modules/clipboard
+    ../modules/notification
+    ../modules/process
+    ../modules/terminal
+    ../modules/vcs
+    ../modules/wallpaper
+    ../modules/session-lock
+    ../modules/browser
+    ../modules/editor
+    ../modules/programming
+    ../modules/vpn
+    ../modules/security
+    ../modules/kubernetes
+    ../modules/service
+    ../modules/file-management
+    ../modules/dictionary
+    ../modules/communication
+    ../modules/i18n
+    ../modules/image
+    ../modules/screenshot
+    ../modules/music
+    ../modules/video
+    ../modules/linux-widget
+    ../modules/llm
+    ../modules/virtualisation
 
-      # Extra modules based on private setup.
-      inputs.nix-config-private.user-modules.sops
-      inputs.nix-config-private.user-modules.email
+    # Extra modules based on private setup.
+    inputs.nix-config-private.user-modules.sops
+    inputs.nix-config-private.user-modules.email
+  ];
+
+  ###----------------------------------------
+  ##   Module related options
+  #------------------------------------------
+  bar.waybar.enable = true;
+  kubernetes.extra.enable = true;
+  communication.slack.enable = true;
+  communication.signal.enable = true;
+  communication.discord.enable = true;
+  communication.zoom.enable = true;
+  notification.swaync.enable = true;
+  service.surrealdb.enable = true;
+  window-manager.hyprland.enable = true;
+  window-manager.niri.enable = true;
+
+  ###----------------------------------------
+  ##   Other Home Manager Setup
+  #------------------------------------------
+  programs.home-manager.enable = true;
+  xdg.enable = true;
+
+  home = {
+    username = "${username}";
+    homeDirectory = "/home/${username}";
+
+    packages = [
+      # Utility
+      pkgs.mesa-demos # For OpenGL etc.
     ];
 
-    ###----------------------------------------
-    ##   Module related options
-    #------------------------------------------
-    bar.waybar.enable = true;
-    kubernetes.extra.enable = true;
-    communication.slack.enable = true;
-    communication.signal.enable = true;
-    communication.discord.enable = true;
-    communication.zoom.enable = true;
-    notification.swaync.enable = true;
-    service.surrealdb.enable = true;
-    window-manager.hyprland.enable = true;
-    window-manager.niri.enable = true;
-
-    ###----------------------------------------
-    ##   Other Home Manager Setup
-    #------------------------------------------
-    programs.home-manager.enable = true;
-    xdg.enable = true;
-
-    home = {
-      username = "${username}";
-      homeDirectory = "/home/${username}";
-
-      packages = [
-        # Utility
-        pkgs.mesa-demos # For OpenGL etc.
-      ];
-
-      stateVersion = "23.11";
-    };
-  }
+    stateVersion = "23.11";
+  };
+}

c844697d047b7e626c8240acc517b7c2a1a871a6
Ryota <rytswd@gmail.com>
2025-12-27 00:56:20 -0500

    Correct address

--- user-config/admin/home-git-clone.nix
+++ user-config/admin/home-git-clone.nix
@@ -1,7 +1,7 @@
 {
   home.gitClone = {
     "Coding/github.com/rytswd/nix-config" = {
-      url = "https://github.com:rytswd/nix-config.git";
+      url = "https://github.com/rytswd/nix-config.git";
       rev = "main";
       # useWorktree = false;  # Default: clone to Coding/.../emacs-config/
       update = true;          # Pull updates on each activation

9ee47cf27a83285b3bd32d6b282281f924bc20e4
Ryota <rytswd@gmail.com>
2026-01-19 11:28:01 +0000

    Correct niri's overlay setup

--- nixos-config/modules/window-manager/niri.nix
+++ nixos-config/modules/window-manager/niri.nix
@@ -1,25 +1,32 @@
-{ pkgs
-, lib
-, config
-, ...}:
+{
+  pkgs,
+  lib,
+  config,
+  inputs,
+  ...
+}:
 
 {
   options = {
     window-manager.niri.enable = lib.mkEnableOption "Enable Niri window manager.";
   };
 
-  config = lib.mkIf config.window-manager.niri.enable {
-    environment.systemPackages = [
-      pkgs.niri-unstable
-    ];
+  config =
+    let
+      niri-unstable = inputs.niri.packages.${pkgs.stdenv.hostPlatform.system}.niri-unstable;
+    in
+    lib.mkIf config.window-manager.niri.enable {
+      environment.systemPackages = [
+        niri-unstable
+      ];
 
-    # NOTE: This can only be enabled once built with this stanza commented out.
-    programs.niri = {
-      enable = true;
-      package = pkgs.niri-unstable;
+      # NOTE: This can only be enabled once built with this stanza commented out.
+      programs.niri = {
+        enable = true;
+        package = niri-unstable;
+      };
+
+      # Default to niri as the default session.
+      services.displayManager.defaultSession = "niri";
     };
-
-    # Default to niri as the default session.
-    services.displayManager.defaultSession = "niri";
-  };
 }

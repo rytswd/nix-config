01b1eb46409f9a138f95983c67663a2aaee6a673
Ryota <rytswd@gmail.com>
2026-01-22 11:13:23 +0000

    Update Dropbox setup to include Maestral config
    This uses SOPS Nix to pull out the account information.

--- user-config/modules/file-management/dropbox.nix
+++ user-config/modules/file-management/dropbox.nix
@@ -8,15 +8,39 @@
     file-management.dropbox.enable = lib.mkEnableOption "Enable Dropbox.";
   };
 
-  config = lib.mkIf config.file-management.dropbox.enable {
-    home.packages =
-      if pkgs.stdenv.isDarwin
-      then []
-      # NOTE: I couldn't make Dropbox to work in Linux env. For now, Maestral
-      # does what I need.
-      else [ pkgs.maestral ]
-    ;
+  config = let
+    # Base setup
+    base = lib.mkIf config.file-management.dropbox.enable {
+      home.packages =
+        if pkgs.stdenv.isDarwin
+        then []
+        # NOTE: I couldn't make Dropbox to work in Linux env. For now, Maestral
+        # does what I need.
+        else [ pkgs.maestral ];
 
-    # TODO: Configure systemd to start maestral daemon.
-  };
+      # TODO: Configure systemd to start maestral daemon.
+    };
+
+    # SOPS Nix based secret handling
+    withSops = lib.mkIf config.security.sops-nix.enable {
+      # Use sops.templates to generate the config with secrets substituted.
+      sops.templates."maestral-config" = {
+        # With file input like this, the file is expected to have the following
+        # placeholder string:
+        #
+        #     <SOPS:**SHA256_OF_SECRET**:PLACEHOLDER>
+        #
+        # And SHA256 can be generated using
+        #
+        #     nix repl
+        #     > builtins.hashString "sha256" "SECRET_NAME"
+        #
+        file = ./maesral.ini;
+        path = "${config.xdg.configHome}/maestral/maestral.ini";
+      };
+    };
+  in lib.mkMerge [
+    base
+    withSops
+  ];
 }

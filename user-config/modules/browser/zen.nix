c6bfe8fe6fd3ef4513a836f16f1da85a4619677b
Ryota <rytswd@gmail.com>
2026-01-22 15:27:09 +0000

    Add a note about rsync

--- user-config/modules/browser/zen.nix
+++ user-config/modules/browser/zen.nix
@@ -14,5 +14,10 @@
       # inputs.zen-browser.packages."${pkgs.stdenv.hostPlatform.system}".beta
       inputs.zen-browser.packages."${pkgs.stdenv.hostPlatform.system}".twilight
     ];
+
+    # NOTE: Zen Browser does not have config sync between machines. For now, I'm
+    # simply copying the config over using rsync.
+    #
+    #    rsync -avz -e "ssh -p 8422" ~/.zen/ user@REMOTE:~/.zen/
   };
 }

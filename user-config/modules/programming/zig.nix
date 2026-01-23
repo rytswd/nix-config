eb330facdda7b599882bd22e361a42619eb955cd
Ryota <rytswd@gmail.com>
2026-01-23 02:28:52 +0000

    Downgrade zls as it doesn't compile with v0.16

--- user-config/modules/programming/zig.nix
+++ user-config/modules/programming/zig.nix
@@ -13,9 +13,10 @@
     home.packages = let
       # NOTE: Based on https://github.com/mitchellh/zig-overlay
       zig = pkgs.zigpkgs.master;
-      zls = inputs.zls.packages.${pkgs.stdenv.hostPlatform.system}.zls.overrideAttrs (_: {
-        nativeBuildInputs = [ zig ];
-      });
+      # zls = inputs.zls.packages.${pkgs.stdenv.hostPlatform.system}.zls.overrideAttrs (_: {
+      #   nativeBuildInputs = [ zig ];
+      # });
+      zls = pkgs.zls; # NOTE: This is older than zig itself.
     in [
       # pkgs.zig
       zig

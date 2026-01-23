8429670b2d1e0d2cd330b130e95fd657d92ece01
Ryota <rytswd@gmail.com>
2026-01-23 01:47:07 +0000

    Remove incorrect mapping

--- nixos-config/modules/nix-impermanence.nix
+++ nixos-config/modules/nix-impermanence.nix
@@ -76,11 +76,6 @@
         "/etc/ssh/ssh_host_rsa_key"
         "/etc/ssh/ssh_host_rsa_key.pub"
       ];
-      # Setting up users who shouldn't lose the home directory.
-      users = {
-        admin.directories = [];
-        ryota.directories = [];
-      };
     };
   };
 }

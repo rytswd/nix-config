551e67c9c13f080103a53efe20d2b8da6cb64675
Ryota <rytswd@gmail.com>
2026-02-11 15:23:36 +0000

    Limit generation with limine

--- nixos-config/modules/core/boot/limine.nix
+++ nixos-config/modules/core/boot/limine.nix
@@ -22,6 +22,9 @@
         # Ref: https://github.com/limine-bootloader/limine/blob/v9.x/CONFIG.md
         enable = true;
 
+        # Keeping only a few generations.
+        maxGenerations = 3;
+
         style.wallpapers = [ ./hasliberg.jpg ];
       };
     };

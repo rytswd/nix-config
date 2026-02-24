23d5381a90a2b4b1177d98419bc7c9ad4b3116de
Ryota <rytswd@gmail.com>
2026-02-24 00:19:20 +0000

    Clarify and reword rust setup

--- user-config/modules/programming/rust.nix
+++ user-config/modules/programming/rust.nix
@@ -11,13 +11,15 @@
   config = lib.mkIf config.programming.rust.enable {
     home.packages = [
       # NOTE: Rust setup is either to use Nix based build setup, or rely on
-      # rustup. Because when I really need to get a version controlled Rust,
-      # it would be based on direnv setup, I'm using rustup as the default
-      # instead. Nix setup here only handles the installation on rustup CLI
-      # itself, and the rest is managed outside of Nix for simplicity.
+      # rustup. When I really need to get a version controlled Rust,
+      # I would use direnv based setup. For that reason, rustup is the default
+      # setup I have. Nix setup here only handles the installation on rustup
+      # CLI itself, and the rest is managed outside of Nix for simplicity.
+      #
       # For rust-analyzer, I needed to run the following:
+      #
       #   `rustup component add rust-analyzer`
-      # Not sure if this is a hard requirement, but this seems to work.
+      #
       pkgs.rustup
       # pkgs.rustc
       # pkgs.cargo

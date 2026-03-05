5930ca820b5b7ef0d57348b95d93cabeac0d9c8b
Ryota <rytswd@gmail.com>
2026-03-05 01:04:10 +0000

    Adjust lid switch handling during power

--- nixos-config/modules/machine-specific/laptop.nix
+++ nixos-config/modules/machine-specific/laptop.nix
@@ -1,16 +1,14 @@
-{ pkgs
-, lib
-, config
-, ...}:
+{
+  pkgs,
+  lib,
+  config,
+  ...
+}:
 
 # Because this module is specific to laptop, the import of this module would
 # directly enable the laptop related configuration.
 
 {
-  systemd.sleep.extraConfig = ''
-    HibernateDelaySec=1h
-  '';
-
   # Ref: https://discourse.nixos.org/t/battery-life-still-isnt-great/41188/3
   # TODO: I need to review this more closely, it's working OK but could be better.
   powerManagement = {
@@ -19,6 +17,10 @@
     cpuFreqGovernor = "powersave";
   };
   services = {
+    logind.settings.Login = {
+      HandleLidSwitch = "suspend"; # on battery → sleep
+      HandleLidSwitchExternalPower = "ignore"; # on AC → do nothing (keep running)
+    };
     thermald.enable = true;
     power-profiles-daemon.enable = false;
     auto-cpufreq = {

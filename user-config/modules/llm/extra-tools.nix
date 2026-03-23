143ec8ed2e6d306b3357b6a60554184fac0b976d
Ryota <rytswd@gmail.com>
2026-03-23 21:58:24 +0100

    Add workmux config with pi

--- user-config/modules/llm/extra-tools.nix
+++ user-config/modules/llm/extra-tools.nix
@@ -13,5 +13,8 @@
     home.packages = with inputs.llm-agents.packages.${pkgs.stdenv.hostPlatform.system}; [
       workmux
     ];
+    xdg.configFile = {
+      "workmux/config.yaml".source = ./workmux.yaml;
+    };
   };
 }

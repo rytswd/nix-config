c1b0bb0812a70865366d47f7c5d47a67422d6424
Ryota <rytswd@gmail.com>
2026-05-27 12:53:51 +0100

    Adjust process config

--- user-config/modules/process/default.nix
+++ user-config/modules/process/default.nix
@@ -1,12 +1,5 @@
-{ pkgs
-, lib
-, config
-, ...}:
-
 {
   imports = [
     ./pueue.nix
   ];
-
-  process.pueue.enable = lib.mkDefault true;
 }
--- user-config/modules/process/pueue.nix
+++ user-config/modules/process/pueue.nix
@@ -1,16 +1,4 @@
-{ pkgs
-, lib
-, config
-, ...}:
-
 {
-  options = {
-    process.pueue.enable = lib.mkEnableOption "Enable Pueue.";
-  };
-
-  config = lib.mkIf config.process.pueue.enable {
-    services.pueue = {
-      enable = true;
-    };
-  };
+  # https://github.com/Nukesor/pueue — task queue / job runner.
+  services.pueue.enable = true;
 }

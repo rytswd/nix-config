10ca822bc95d92d0b3651cb124b347a6b4b1f67d
Ryota <rytswd@gmail.com>
2026-05-29 13:54:40 +0100

    Move gh into product module

--- user-config/modules/product/vcs/github.nix
+++ user-config/modules/product/vcs/github.nix
@@ -1,16 +1,3 @@
-{ pkgs
-, lib
-, config
-, ...}:
-
 {
-  options = {
-    service.github.enable = lib.mkEnableOption "Enable GitHub related tooling.";
-  };
-
-  config = lib.mkIf config.service.github.enable {
-    programs.gh = {
-      enable = true;
-    };
-  };
+  programs.gh.enable = true;
 }

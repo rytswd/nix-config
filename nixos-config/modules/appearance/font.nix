995002b3eea17dc44935c9abb695940b66828608
Ryota <rytswd@gmail.com>
2026-05-29 17:25:58 +0100

    Adjust indentation

--- nixos-config/modules/appearance/font.nix
+++ nixos-config/modules/appearance/font.nix
@@ -1,37 +1,37 @@
 { pkgs, ... }:
 {
   fonts = {
-      fontDir.enable = true;
+    fontDir.enable = true;
 
-      packages = [
-        pkgs.nerd-fonts.droid-sans-mono
-        pkgs.nerd-fonts.fira-code
-        pkgs.nerd-fonts.fira-mono
-        pkgs.nerd-fonts.hack
-        pkgs.nerd-fonts.iosevka
-        pkgs.nerd-fonts.noto
-        pkgs.nerd-fonts.symbols-only
-        pkgs.nerd-fonts.victor-mono
+    packages = [
+      pkgs.nerd-fonts.droid-sans-mono
+      pkgs.nerd-fonts.fira-code
+      pkgs.nerd-fonts.fira-mono
+      pkgs.nerd-fonts.hack
+      pkgs.nerd-fonts.iosevka
+      pkgs.nerd-fonts.noto
+      pkgs.nerd-fonts.symbols-only
+      pkgs.nerd-fonts.victor-mono
 
-        pkgs.dejavu_fonts
-        pkgs.raleway
-        pkgs.monaspace
-        pkgs.noto-fonts
-        pkgs.noto-fonts-cjk-sans
-        pkgs.noto-fonts-color-emoji
-        pkgs.victor-mono
-        pkgs.emacs-all-the-icons-fonts
+      pkgs.dejavu_fonts
+      pkgs.raleway
+      pkgs.monaspace
+      pkgs.noto-fonts
+      pkgs.noto-fonts-cjk-sans
+      pkgs.noto-fonts-color-emoji
+      pkgs.victor-mono
+      pkgs.emacs-all-the-icons-fonts
 
-        # Fonts from Sora Sagano, clean and sophisticated look
-        # Ref: https://dotcolon.net
-        pkgs.tenderness
-        pkgs.medio
-        pkgs.melete
-        pkgs.seshat
-        pkgs.penna
-        pkgs.fa_1
-        pkgs.nacelle
-        pkgs.route159
-      ];
+      # Fonts from Sora Sagano, clean and sophisticated look
+      # Ref: https://dotcolon.net
+      pkgs.tenderness
+      pkgs.medio
+      pkgs.melete
+      pkgs.seshat
+      pkgs.penna
+      pkgs.fa_1
+      pkgs.nacelle
+      pkgs.route159
+    ];
   };
 }

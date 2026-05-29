91cb90d3847f3a15711e48306898593e7ab1dbf0
Ryota <rytswd@gmail.com>
2026-05-29 16:54:35 +0100

    Adjust indentation

--- user-config/modules/shell/util.nix
+++ user-config/modules/shell/util.nix
@@ -5,55 +5,55 @@
     ##   Network related
     #--------------------------------
     pkgs.tcpdump
-    pkgs.bandwhich # https://github.com/imsnif/bandwhich
+    pkgs.bandwhich    # https://github.com/imsnif/bandwhich
 
     ###------------------------------
     ##   File related
     #--------------------------------
-    pkgs.eza # https://github.com/eza-community/eza
-    pkgs.bat # https://github.com/sharkdp/bat
-    pkgs.ripgrep # https://github.com/BurntSushi/ripgrep
-    pkgs.fd # https://github.com/sharkdp/fd
-    pkgs.zoxide # https://github.com/ajeetdsouza/zoxide
-    pkgs.dust # https://github.com/bootandy/dust
-    pkgs.erdtree # https://github.com/solidiquis/erdtree
-    pkgs.delta # https://github.com/dandavison/delta
-    pkgs.difftastic # https://github.com/Wilfred/difftastic
-    pkgs.cloc # https://github.com/AlDanial/cloc
-    pkgs.tokei # https://github.com/XAMPPRocky/tokei
-    pkgs.ripunzip # https://github.com/google/ripunzip
-    pkgs.moor # https://github.com/walles/moor
+    pkgs.eza          # https://github.com/eza-community/eza
+    pkgs.bat          # https://github.com/sharkdp/bat
+    pkgs.ripgrep      # https://github.com/BurntSushi/ripgrep
+    pkgs.fd           # https://github.com/sharkdp/fd
+    pkgs.zoxide       # https://github.com/ajeetdsouza/zoxide
+    pkgs.dust         # https://github.com/bootandy/dust
+    pkgs.erdtree      # https://github.com/solidiquis/erdtree
+    pkgs.delta        # https://github.com/dandavison/delta
+    pkgs.difftastic   # https://github.com/Wilfred/difftastic
+    pkgs.cloc         # https://github.com/AlDanial/cloc
+    pkgs.tokei        # https://github.com/XAMPPRocky/tokei
+    pkgs.ripunzip     # https://github.com/google/ripunzip
+    pkgs.moor         # https://github.com/walles/moor
 
     # pkgs.rm-improved  # https://github.com/nivekuil/rip
 
     ###------------------------------
     ##   Security related
     #--------------------------------
-    pkgs.mkcert # https://github.com/FiloSottile/mkcert
+    pkgs.mkcert       # https://github.com/FiloSottile/mkcert
     pkgs.openssl
 
     ###------------------------------
     ##   Performance related
     #--------------------------------
-    pkgs.btop # https://github.com/aristocratos/btop
-    pkgs.htop # https://github.com/htop-dev/htop
-    pkgs.bottom # https://github.com/ClementTsang/bottom
-    pkgs.hyperfine # https://github.com/sharkdp/hyperfine
-    pkgs.powertop # https://github.com/fenrus75/powertop
+    pkgs.btop         # https://github.com/aristocratos/btop
+    pkgs.htop         # https://github.com/htop-dev/htop
+    pkgs.bottom       # https://github.com/ClementTsang/bottom
+    pkgs.hyperfine    # https://github.com/sharkdp/hyperfine
+    pkgs.powertop     # https://github.com/fenrus75/powertop
 
     ###------------------------------
     ##   Process related
     #--------------------------------
-    pkgs.procs # https://github.com/dalance/procs
-    pkgs.pueue # https://github.com/Nukesor/pueue
+    pkgs.procs        # https://github.com/dalance/procs
+    pkgs.pueue        # https://github.com/Nukesor/pueue
 
     ###------------------------------
     ##   Other
     #--------------------------------
-    pkgs.shellcheck # https://github.com/koalaman/shellcheck
-    pkgs.atuin # https://github.com/atuinsh/atuin
-    pkgs.fastfetch # https://github.com/fastfetch-cli/fastfetch
-    pkgs.asciinema_3 # https://github.com/asciinema/asciinema
+    pkgs.shellcheck   # https://github.com/koalaman/shellcheck
+    pkgs.atuin        # https://github.com/atuinsh/atuin
+    pkgs.fastfetch    # https://github.com/fastfetch-cli/fastfetch
+    pkgs.asciinema_3  # https://github.com/asciinema/asciinema
     pkgs.asciinema-agg # https://github.com/asciinema/agg
 
     # pkgs.zellij       # https://github.com/zellij-org/zellij

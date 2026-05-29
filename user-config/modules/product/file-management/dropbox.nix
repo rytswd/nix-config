aa1c9e040bbceb5fd9555ab70499fddecad5314e
Ryota <rytswd@gmail.com>
2026-05-29 13:54:25 +0100

    Add dropbox setup in product module

--- user-config/modules/product/file-management/dropbox.nix
+++ user-config/modules/product/file-management/dropbox.nix
@@ -1,46 +1,31 @@
-{ pkgs
-, lib
-, config
-, ...}:
-
+{ pkgs, config, ... }:
+# Assumes the security bundle (which always imports sops-nix.nix) is also
+# imported by the host — sops.templates below relies on the sops module.
 {
-  options = {
-    rytswd.file-management.dropbox.enable = lib.mkEnableOption "Enable Dropbox.";
+  home.packages =
+    if pkgs.stdenv.isDarwin then
+      [ ]
+    # NOTE: I couldn't make Dropbox to work in Linux env. For now, Maestral
+    # does what I need.
+    else
+      [ pkgs.maestral ];
+
+  # TODO: Configure systemd to start maestral daemon.
+
+  # SOPS Nix based secret handling.
+  # Use sops.templates to generate the config with secrets substituted.
+  sops.templates."maestral-config" = {
+    # With file input like this, the file is expected to have the following
+    # placeholder string:
+    #
+    #     <SOPS:**SHA256_OF_SECRET**:PLACEHOLDER>
+    #
+    # And SHA256 can be generated using
+    #
+    #     nix repl
+    #     > builtins.hashString "sha256" "SECRET_NAME"
+    #
+    file = ./maestral.ini;
+    path = "${config.xdg.configHome}/maestral/maestral.ini";
   };
-
-  config = let
-    # Base setup
-    base = lib.mkIf config.rytswd.file-management.dropbox.enable {
-      home.packages =
-        if pkgs.stdenv.isDarwin
-        then []
-        # NOTE: I couldn't make Dropbox to work in Linux env. For now, Maestral
-        # does what I need.
-        else [ pkgs.maestral ];
-
-      # TODO: Configure systemd to start maestral daemon.
-    };
-
-    # SOPS Nix based secret handling
-    withSops = lib.mkIf config.security.sops-nix.enable {
-      # Use sops.templates to generate the config with secrets substituted.
-      sops.templates."maestral-config" = {
-        # With file input like this, the file is expected to have the following
-        # placeholder string:
-        #
-        #     <SOPS:**SHA256_OF_SECRET**:PLACEHOLDER>
-        #
-        # And SHA256 can be generated using
-        #
-        #     nix repl
-        #     > builtins.hashString "sha256" "SECRET_NAME"
-        #
-        file = ./maestral.ini;
-        path = "${config.xdg.configHome}/maestral/maestral.ini";
-      };
-    };
-  in lib.mkMerge [
-    base
-    withSops
-  ];
 }

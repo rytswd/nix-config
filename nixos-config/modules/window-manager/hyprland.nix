34c2e528b5207b69b89ed3eed34ce64284a0263c
Ryota <rytswd@gmail.com>
2026-05-29 01:54:33 +0100

    Adjust window manager setup

--- nixos-config/modules/window-manager/hyprland.nix
+++ nixos-config/modules/window-manager/hyprland.nix
@@ -1,14 +1,3 @@
-{ pkgs
-, lib
-, config
-, ...}:
-
 {
-  options = {
-    window-manager.hyprland.enable = lib.mkEnableOption "Enable hyprland.";
-  };
-
-  config = lib.mkIf config.window-manager.hyprland.enable {
-    programs.hyprland.enable = true;
-  };
+  programs.hyprland.enable = true;
 }
--- nixos-config/modules/window-manager/niri.nix
+++ nixos-config/modules/window-manager/niri.nix
@@ -1,32 +1,23 @@
 {
   pkgs,
-  lib,
-  config,
   inputs,
   ...
 }:
 
+let
+  niri-unstable = inputs.niri.packages.${pkgs.stdenv.hostPlatform.system}.niri-unstable;
+in
 {
-  options = {
-    window-manager.niri.enable = lib.mkEnableOption "Enable Niri window manager.";
+  environment.systemPackages = [
+    niri-unstable
+  ];
+
+  # NOTE: This can only be enabled once built with this stanza commented out.
+  programs.niri = {
+    enable = true;
+    package = niri-unstable;
   };
 
-  config =
-    let
-      niri-unstable = inputs.niri.packages.${pkgs.stdenv.hostPlatform.system}.niri-unstable;
-    in
-    lib.mkIf config.window-manager.niri.enable {
-      environment.systemPackages = [
-        niri-unstable
-      ];
-
-      # NOTE: This can only be enabled once built with this stanza commented out.
-      programs.niri = {
-        enable = true;
-        package = niri-unstable;
-      };
-
-      # Default to niri as the default session.
-      services.displayManager.defaultSession = "niri";
-    };
+  # Default to niri as the default session.
+  services.displayManager.defaultSession = "niri";
 }

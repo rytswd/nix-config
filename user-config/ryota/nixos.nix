7d355521be367c9ab37f00d172d51a9c4f515c22
Ryota <rytswd@gmail.com>
2026-06-02 01:07:11 +0100

    Clean up nixos module setup

--- user-config/ryota/nixos.nix
+++ user-config/ryota/nixos.nix
@@ -16,31 +16,31 @@ in
   imports = [
     # Shared machine-local values (e.g. `local.repoPath`).
     "${self}/user-config/modules/lib/paths.nix"
+    # Home-manager bootstrap (CLI install + release-check suppression).
+    "${self}/user-config/modules/home-manager"
 
     # The shell setup defines some aliases, and in order to allow overriding,
     # calling this earlier than other modules.
     "${self}/user-config/modules/shell"
+
     # The rest of the module call order is rather arbitrary, just in order of
     # importance for my own use case.
     "${self}/user-config/modules/key-remap/xremap"
+    "${self}/user-config/modules/window-manager/niri"
+    "${self}/user-config/modules/window-manager/hyprland" # Backup, not really used
+
     "${self}/user-config/modules/appearance"
-    "${self}/user-config/modules/window-manager"
     "${self}/user-config/modules/launcher"
-    "${self}/user-config/modules/bar"
     "${self}/user-config/modules/clipboard"
     "${self}/user-config/modules/notification"
     "${self}/user-config/modules/process"
     "${self}/user-config/modules/terminal"
     "${self}/user-config/modules/vcs"
-    "${self}/user-config/modules/wallpaper"
-    # ../modules/session-lock # Commenting out as Noctalia handles this.
     "${self}/user-config/modules/browser"
     "${self}/user-config/modules/editor"
     "${self}/user-config/modules/programming"
     "${self}/user-config/modules/vpn"
     "${self}/user-config/modules/security"
-    "${self}/user-config/modules/kubernetes"
-    "${self}/user-config/modules/service"
     "${self}/user-config/modules/file-management"
     "${self}/user-config/modules/dictionary"
     "${self}/user-config/modules/communication"
@@ -53,6 +53,16 @@ in
     "${self}/user-config/modules/llm"
     "${self}/user-config/modules/virtualisation"
 
+    "${self}/user-config/modules/database/surrealdb.nix"
+    "${self}/user-config/modules/kubernetes"
+    "${self}/user-config/modules/kubernetes/kubernetes-extra.nix"
+
+    # Vendor solutions
+    "${self}/user-config/modules/product/cloud"
+    "${self}/user-config/modules/product/security"
+    "${self}/user-config/modules/product/vcs"
+    "${self}/user-config/modules/product/collaboration"
+
     # Extra modules based on private setup.
     inputs.nix-config-private.homeManagerModules.sops-nix
     inputs.nix-config-private.homeManagerModules.email
@@ -63,39 +73,9 @@ in
     ./default-apps.nix
   ];
 
-  ###----------------------------------------
-  ##   Module related options
-  #------------------------------------------
-  # bar.waybar.enable = true;
-  kubernetes.extra.enable = true;
-  communication.slack.enable = true;
-  communication.signal.enable = true;
-  communication.discord.enable = true;
-  communication.telegram.enable = true;
-  communication.zoom.enable = true;
-  # notification.swaync.enable = true;
-  service.surrealdb.enable = true;
-  window-manager.hyprland.enable = true;
-  window-manager.niri.enable = true;
-
-  ###----------------------------------------
-  ##   Other Home Manager Setup
-  #------------------------------------------
-  programs.home-manager.enable = true;
-  xdg.enable = true;
-
   home = {
-    # I specifically use different version for system and home.
-    enableNixpkgsReleaseCheck = false;
-
     username = "${username}";
     homeDirectory = "/home/${username}";
-
-    packages = [
-      # Utility
-      pkgs.mesa-demos # For OpenGL etc.
-    ];
-
     stateVersion = "23.11";
   };
 }

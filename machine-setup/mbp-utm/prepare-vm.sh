c78f7c709b08f5189368bf116c6d6eb866e74df7
Ryota <rytswd@gmail.com>
2026-06-10 22:02:52 +0100

    Adjust prepare-vm script

--- machine-setup/mbp-utm/prepare-vm.sh
+++ machine-setup/mbp-utm/prepare-vm.sh
@@ -44,11 +44,10 @@ ssh -o PubkeyAuthentication=no \
 		mount /dev/disk/by-label/boot /mnt/boot; \
 		nixos-generate-config --root /mnt; \
 		sed --in-place '/system\.stateVersion = .*/a \
-			nix.package = pkgs.nixUnstable;\n \
-			nix.extraOptions = \"experimental-features = nix-command flakes\";\n \
-  			services.openssh.enable = true;\n \
-			services.openssh.passwordAuthentication = true;\n \
-			services.openssh.permitRootLogin = \"yes\";\n \
+			nix.settings.experimental-features = [ \"nix-command\" \"flakes\" ];\n \
+			services.openssh.enable = true;\n \
+			services.openssh.settings.PasswordAuthentication = true;\n \
+			services.openssh.settings.PermitRootLogin = \"yes\";\n \
 			users.users.root.initialPassword = \"root\";\n \
 		' /mnt/etc/nixos/configuration.nix; \
 		nixos-install --no-root-passwd; \

9d30941b8ed8f5348ec2717fa2cb72c4eacc6055
Ryota <rytswd@gmail.com>
2026-08-30 03:16:40 +0100

    Fix firefox warning

--- user-config/modules/browser/firefox.nix
+++ user-config/modules/browser/firefox.nix
@@ -1,6 +1,7 @@
-{ pkgs, ... }:
+{ pkgs, config, ... }:
 {
   programs.firefox.enable = true;
+  programs.firefox.configPath = "${config.xdg.configHome}/mozilla/firefox";
   home.packages = [
     pkgs.librewolf
   ];
